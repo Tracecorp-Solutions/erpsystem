90890969eb6acb1fb90b46e731d848492774b8df
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-04-22 11:12:58 +0300

    Add dashboard files

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+// import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+// import Footer from './components/Footer';
+
+function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/projects')
+      .then(response => response.json())
+      .then(data => setProjects(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div className="min-h-full">
+      {/* <Navbar /> */}
+
+      <div className="py-">
+        <Header />
+        <main>
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            
+          </div>
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
