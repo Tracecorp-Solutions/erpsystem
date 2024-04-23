7ddc9e4cdff9c9370c9cf3c4cb39e8c6fe3c04d2
unknown <kinyeramo@gmail.com>
2024-04-23 10:28:38 +0300

    remove unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Routers } from 'react-router-dom';
 import './App.css';
-// import Navbar from './components/Navbar';
 import Main from './components/Header';
 
 
