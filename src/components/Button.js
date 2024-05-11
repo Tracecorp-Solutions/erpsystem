428f56137bfe87eee4591ed9dfa9d6fa41322a75
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-05-11 22:59:48 +0300

    add button radius

--- src/components/Button.js
+++ src/components/Button.js
@@ -49,7 +49,7 @@ export default function NavigationHeader() {
             <div>
               <button
                 type="button"
-                className="inline-flex items-center rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+                className="inline-flex items-center rounded-xl bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
                 id="menu-button"
                 style={{
                   fontFamily: "outfit, sans-serif",
