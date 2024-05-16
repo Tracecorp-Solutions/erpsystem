7b1fa49aa20dde29b7f43b2ba2407f3371f5986d
unknown <kinyeramo@gmail.com>
2024-05-16 14:08:40 +0300

    fix the issues

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -4,6 +4,7 @@ import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import axios from "axios";
 import AccountNavigationFilter from "../components/SubGroupNavigationFilter";
 import FailureSlideInCard from "../components/FailureSlideInCard ";
+import ReusableEmptyData from "../components/ReusableEmptyData";
 
 const Customer = () => {
   const [customerList, setCustomerList] = useState([]);
@@ -38,95 +39,105 @@ const Customer = () => {
     </Menu>
   );
 
-//   const handleCloseSuccess = () => {
-//     setShowFailure(false);
-//   };
+  //   const handleCloseSuccess = () => {
+  //     setShowFailure(false);
+  //   };
 
   return (
     <div style={{ background: "#fff", padding: "15px", borderRadius: "24px" }}>
       <AccountNavigationFilter />
-      {showFailure && <FailureSlideInCard title={messageInfo.title} message={messageInfo.message} onClose={() => setShowFailure(false)}/>}
-      <div style={{ overflowY: "auto" }}>
-        <table className="table-auto min-w-full divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr style={{ borderRadius: "50px" }}>
-              <input
-                type="checkbox"
-                style={{ marginLeft: "10px", marginTop: "15px" }}
-              />
-              <th
-                scope="col"
-                className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Company Name
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Email
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Phone
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Opening Balance
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                ACTION
-              </th>
-            </tr>
-          </thead>
-          {customerList.map((customer) => (
-            <tbody
-              className="bg-white divide-y divide-gray-200"
-              key={customer.id}
-            >
-              <tr className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
-                <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
-                  <input type="checkbox" />
-                </td>
-                <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
-                  {customer.companyName}
-                </td>
-                <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
-                  {customer.email}
-                </td>
-                <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
-                  {customer.mobile}
-                </td>
-                <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
-                  {customer.openingBalance}
-                </td>
-                <div
-                  style={{
-                    width: "100px",
-                    display: "flex",
-                    justifyContent: "center",
-                    marginTop: "10px",
-                  }}
+      {showFailure && (
+        <FailureSlideInCard
+          title={messageInfo.title}
+          message={messageInfo.message}
+          onClose={() => setShowFailure(false)}
+        />
+      )}
+      {customerList.length < 0 ? (
+        <ReusableEmptyData />
+      ) : (
+        <div style={{ overflowY: "auto" }}>
+          <table className="table-auto min-w-full divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr style={{ borderRadius: "50px" }}>
+                <input
+                  type="checkbox"
+                  style={{ marginLeft: "10px", marginTop: "15px" }}
+                />
+                <th
+                  scope="col"
+                  className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  <Dropdown overlay={menu} trigger={["click"]}>
-                    <EllipsisVerticalIcon
-                      className="h-5 w-5 mt-3"
-                      aria-hidden="true"
-                    />
-                  </Dropdown>
-                </div>
+                  Company Name
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Email
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Phone
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Opening Balance
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  ACTION
+                </th>
               </tr>
-            </tbody>
-          ))}
-        </table>
-      </div>
+            </thead>
+            {customerList.map((customer) => (
+              <tbody
+                className="bg-white divide-y divide-gray-200"
+                key={customer.id}
+              >
+                <tr className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                  <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                    <input type="checkbox" />
+                  </td>
+                  <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                    {customer.companyName}
+                  </td>
+                  <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                    {customer.email}
+                  </td>
+                  <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                    {customer.mobile}
+                  </td>
+                  <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                    {customer.openingBalance}
+                  </td>
+                  <div
+                    style={{
+                      width: "100px",
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <Dropdown overlay={menu} trigger={["click"]}>
+                      <EllipsisVerticalIcon
+                        className="h-5 w-5 mt-3"
+                        aria-hidden="true"
+                      />
+                    </Dropdown>
+                  </div>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      )}
     </div>
   );
 };
