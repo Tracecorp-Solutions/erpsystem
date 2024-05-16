f8a81036c58533265c998aaa558482f530da279b
unknown <kinyeramo@gmail.com>
2024-05-16 21:19:59 +0300

    style the button to match figma design

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -417,8 +417,177 @@ const CustomerForm = ({ showModal }) => {
         )}
         {section === 3 && (
           <div style={{ display: "flex", justifyContent: "space-between" }}>
-            {/* Section 3 form fields */}
-            <Button
+            <div>
+                  <div className="grid max-w-xl w-full mx-4">
+                    {/* {isActive ? (
+                      <div className="mb-3 mr-3">
+                        <label
+                          htmlFor="name"
+                          className="block mb-1"
+                          style={{
+                            fontFamily: "outFit, Sans-serif",
+                            fontSize: "16px",
+                            fontWeight: "400",
+                          }}
+                        >
+                          Active Status
+                        </label>
+                        <p
+                          className="text-gray-600 text-sm mb-1"
+                          style={{ fontFamily: "outFit, Sans-serif" }}
+                        >
+                          Choose an appropriate status (e.g. Active or Inactive)
+                        </p>
+                        <select
+                          id="status"
+                          name="status"
+                          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                          style={{ borderRadius: "12px", padding: "6px" }}
+                        >
+                          <option value="active">Active</option>
+                          <option value="inactive">Inactive</option>
+                        </select>
+                      </div>
+                    ) : (
+                      <div className="mb-3 mr-3">
+                        <label
+                          htmlFor="name"
+                          className="block mb-1"
+                          style={{
+                            fontFamily: "outFit, Sans-serif",
+                            fontSize: "16px",
+                            fontWeight: "400",
+                          }}
+                        >
+                          Inactive Status
+                        </label>
+                        <p
+                          className="text-gray-600 text-sm mb-1"
+                          style={{ fontFamily: "outFit, Sans-serif" }}
+                        >
+                          Choose an appropriate status (e.g. Active or Inactive)
+                        </p>
+                        <select
+                          id="status"
+                          name="status"
+                          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                          style={{ borderRadius: "12px", padding: "6px" }}
+                        >
+                          <option value="active">Active</option>
+                          <option value="inactive">Inactive</option>
+                        </select>
+                      </div>
+                    )} */}
+
+                    <div className="mb-4 mr-3">
+                      <label
+                        htmlFor="subGroupAccountId"
+                        className="block mb-1"
+                        style={{
+                          fontFamily: "outFit, Sans-serif",
+                          fontSize: "16px",
+                          fontWeight: "400",
+                        }}
+                      >
+                        SubGroup
+                      </label>
+                      <p 
+                      className="text-gray-600 text-sm mb-1"
+                      style={{ fontFamily: "outFit, Sans-serif" }}
+                      >Select the subgroup this account belongs to</p>
+                      <select
+                        id="subGroupAccountId"
+                        name="subGroupAccountId"
+                        // value={newAccount.subGroupAccountId}
+                        // onChange={(e) =>
+                        //   setNewAccount({
+                        //     ...newAccount,
+                        //     subGroupAccountId: e.target.value,
+                        //   })
+                        // }
+                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                        style={{ borderRadius: "12px", padding: "15px" }}
+                      >
+                        <option value=""  style={{ fontFamily: "outFit, Sans-serif" }}>Select SubGroup</option>
+                        {/* {subGroupAccounts.map((subGroup) => (
+                          <option
+                            key={subGroup.subGroupAccount.id}
+                            value={subGroup.subGroupAccount.id}
+                          >
+                            {subGroup.subGroupAccount.name}
+                          </option>
+                        ))} */}
+                      </select>
+                    </div>
+                    <div className="mb-4 mr-3">
+                      <label
+                        htmlFor="accountFromId"
+                        
+                        style={{
+                          fontFamily: "outFit, Sans-serif",
+                          fontSize: "16px",
+                          fontWeight: "400",
+                        }}
+                      >
+                        Account From
+                      </label>
+                      <p
+                        className="text-gray-500 text-xs mb-2"
+                        style={{ fontFamily: "outFit, Sans-serif" }}
+                      >
+                        Source of the funds
+                      </p>
+                      <select
+                        id="accountFromId"
+                        // value={newTransaction.name}
+                        // onChange={(e) =>
+                        //   setNewTransaction({
+                        //     ...newTransaction,
+                        //     accountFromId: parseInt(e.target.value),
+                        //   })
+                        // }
+                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                        style={{ borderRadius: "12px", padding: "15px" }}
+                      >
+                        <option value=""  style={{ fontFamily: "outFit, Sans-serif" }}>Select an account</option>
+                        {/* {accounts.map((account) => (
+                          <option key={account.id} value={account.id}>
+                            {account.name}
+                          </option>
+                        ))} */}
+                      </select>
+                    </div>
+
+                    <div className="mb-3 mr-3">
+                      <label
+                        htmlFor="name"
+                        className="block mb-1"
+                        style={{
+                          fontFamily: "outFit, Sans-serif",
+                          fontSize: "16px",
+                          fontWeight: "400",
+                        }}
+                      >
+                        Notes
+                      </label>
+                      <p
+                        className="text-gray-600 text-sm mb-1"
+                        style={{ fontFamily: "outFit, Sans-serif" }}
+                      >
+                        Add a description
+                      </p>
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Please enter account name..."
+                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                        style={{ borderRadius: "12px", padding: "7px" }}
+                      />
+                    </div>
+                    {/* Add other fields for section 1 */}
+                  </div>
+                  <Button
               type="button"
               onClick={handleBack}
               className="py-2 px-4 text-gray-700 rounded focus:outline-none"
@@ -446,6 +615,7 @@ const CustomerForm = ({ showModal }) => {
             >
               Next
             </Button>
+                </div>
           </div>
         )}
         {section === 4 && (
@@ -453,7 +623,7 @@ const CustomerForm = ({ showModal }) => {
             display: "flex",
             justifyContent: "space-between"
           }}>
-            {/* Section 4 form fields */}
+            
             <Button
               type="button"
               onClick={handleBack}
