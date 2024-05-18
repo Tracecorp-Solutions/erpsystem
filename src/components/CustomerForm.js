897788486a76d9cbf27ad7eb1decdf9e38ef5c6d
unknown <kinyeramo@gmail.com>
2024-05-18 14:59:28 +0300

    done with payment accounts

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -4,29 +4,29 @@ import axios from "axios";
 
 const CustomerForm = ({ showModal }) => {
   const [formData, setFormData] = useState({
-    title: '',
-    fullName: '',
-    email: '',
-    companyName: '',
-    phone: '',
-    mobile: '',
-    website: '',
+    title: "",
+    fullName: "",
+    email: "",
+    companyName: "",
+    phone: "",
+    mobile: "",
+    website: "",
     addres: {
-      street: '',
-      city: '',
-      zipCode: '',
-      country: '',
+      street: "",
+      city: "",
+      zipCode: "",
+      country: "",
     },
-    accountNo: '',
+    accountNo: "",
     billingRate: 0,
     openingBalance: 0,
-    openingBalanceDate: '',
-    notes: '',
-    businessIdNo: '',
+    openingBalanceDate: "",
+    notes: "",
+    businessIdNo: "",
     status: true,
     paymentAccount: 0,
     subGroupId: 0,
-    vendorType: 'Vendor',
+    vendorType: "Vendor",
   });
 
   const [section, setSection] = useState(1);
@@ -50,7 +50,7 @@ const CustomerForm = ({ showModal }) => {
         process.env.REACT_APP_API_URL + "/GetAllSubGroupAccounts"
       );
       setSubGroupAccounts(response.data);
-      console.log("sugrrrorororooror", response.data)
+      console.log("sugrrrorororooror", response.data);
     } catch (error) {
       console.error("Error fetching subGroup accounts", error);
     }
@@ -70,8 +70,8 @@ const CustomerForm = ({ showModal }) => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name.includes('.')) {
-      const [mainKey, subKey] = name.split('.');
+    if (name.includes(".")) {
+      const [mainKey, subKey] = name.split(".");
       setFormData((prevData) => ({
         ...prevData,
         [mainKey]: {
@@ -89,12 +89,13 @@ const CustomerForm = ({ showModal }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://3.216.182.63:8095/CreateVendor', formData)
+    axios
+      .post("http://3.216.182.63:8095/CreateVendor", formData)
       .then((response) => {
-        console.log('Form submitted successfully:', response.data);
+        console.log("Form submitted successfully:", response.data);
       })
       .catch((error) => {
-        console.error('Error submitting form:', error);
+        console.error("Error submitting form:", error);
       });
   };
   const handleContinue = () => {
@@ -104,7 +105,6 @@ const CustomerForm = ({ showModal }) => {
   const handleBack = () => {
     setSection(section - 1);
   };
-
 
   return (
     <Modal
@@ -117,96 +117,242 @@ const CustomerForm = ({ showModal }) => {
         <Progress percent={(section / totalSections) * 100} />
       </div>
       <form onSubmit={handleSubmit} className="max-w-lg mx-auto mt-8">
-  <div className="mb-4">
-    <label className="block mb-2">Title</label>
-    <input type="text" name="title" value={formData.title} onChange={handleChange} placeholder="Title" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Full Name</label>
-    <input type="text" name="fullName" value={formData.fullName} onChange={handleChange} placeholder="Full Name" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Email</label>
-    <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Company Name</label>
-    <input type="text" name="companyName" value={formData.companyName} onChange={handleChange} placeholder="Company Name" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Phone</label>
-    <input type="text" name="phone" value={formData.phone} onChange={handleChange} placeholder="Phone" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Mobile</label>
-    <input type="text" name="mobile" value={formData.mobile} onChange={handleChange} placeholder="Mobile" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Website</label>
-    <input type="text" name="website" value={formData.website} onChange={handleChange} placeholder="Website" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Street</label>
-    <input type="text" name="addres.street" value={formData.addres.street} onChange={handleChange} placeholder="Street" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">City</label>
-    <input type="text" name="addres.city" value={formData.addres.city} onChange={handleChange} placeholder="City" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Zip Code</label>
-    <input type="text" name="addres.zipCode" value={formData.addres.zipCode} onChange={handleChange} placeholder="Zip Code" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Country</label>
-    <input type="text" name="addres.country" value={formData.addres.country} onChange={handleChange} placeholder="Country" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Account Number</label>
-    <input type="text" name="accountNo" value={formData.accountNo} onChange={handleChange} placeholder="Account No" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Billing Rate</label>
-    <input type="number" name="billingRate" value={formData.billingRate} onChange={handleChange} placeholder="Billing Rate" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Opening Balance</label>
-    <input type="number" name="openingBalance" value={formData.openingBalance} onChange={handleChange} placeholder="Opening Balance" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Opening Balance Date</label>
-    <input type="datetime-local" name="openingBalanceDate" value={formData.openingBalanceDate} onChange={handleChange} className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Notes</label>
-    <textarea name="notes" value={formData.notes} onChange={handleChange} placeholder="Notes" className="w-full px-3 py-2 border rounded-md"></textarea>
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Business ID No</label>
-    <input type="text" name="businessIdNo" value={formData.businessIdNo} onChange={handleChange} placeholder="Business ID No" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Status</label>
-    <input type="checkbox" name="status" checked={formData.status} onChange={(e) => setFormData({ ...formData, status: e.target.checked })} className="form-checkbox h-5 w-5 text-blue-600" />
-  </div>
-  <div className="mb-4">
-    <label className="block mb-2">Payment Account</label>
-    <input type="number" name="paymentAccount" value={formData.paymentAccount} onChange={handleChange} placeholder="Payment Account" className="w-full px-3 py-2 border rounded-md" />
-  </div>
-  <div className="mb-4">
-  <label className="block mb-2">Sub Group ID</label>
-  <select name="subGroupId" value={formData.subGroupId} onChange={handleChange} className="w-full px-3 py-2 border rounded-md">
-    {subGroupAccounts.map((subGroup, index) => (
-      <option key={index} value={subGroup.subGroupAccount.id}>{subGroup.subGroupAccount.name}</option>
-    ))}
-  </select>
-</div>
+        <div className="mb-4">
+          <label className="block mb-2">Title</label>
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            placeholder="Title"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Full Name</label>
+          <input
+            type="text"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            placeholder="Full Name"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Email"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Company Name</label>
+          <input
+            type="text"
+            name="companyName"
+            value={formData.companyName}
+            onChange={handleChange}
+            placeholder="Company Name"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            placeholder="Phone"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Mobile</label>
+          <input
+            type="text"
+            name="mobile"
+            value={formData.mobile}
+            onChange={handleChange}
+            placeholder="Mobile"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Website</label>
+          <input
+            type="text"
+            name="website"
+            value={formData.website}
+            onChange={handleChange}
+            placeholder="Website"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Street</label>
+          <input
+            type="text"
+            name="addres.street"
+            value={formData.addres.street}
+            onChange={handleChange}
+            placeholder="Street"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">City</label>
+          <input
+            type="text"
+            name="addres.city"
+            value={formData.addres.city}
+            onChange={handleChange}
+            placeholder="City"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Zip Code</label>
+          <input
+            type="text"
+            name="addres.zipCode"
+            value={formData.addres.zipCode}
+            onChange={handleChange}
+            placeholder="Zip Code"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Country</label>
+          <input
+            type="text"
+            name="addres.country"
+            value={formData.addres.country}
+            onChange={handleChange}
+            placeholder="Country"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Account Number</label>
+          <input
+            type="text"
+            name="accountNo"
+            value={formData.accountNo}
+            onChange={handleChange}
+            placeholder="Account No"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Billing Rate</label>
+          <input
+            type="number"
+            name="billingRate"
+            value={formData.billingRate}
+            onChange={handleChange}
+            placeholder="Billing Rate"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Opening Balance</label>
+          <input
+            type="number"
+            name="openingBalance"
+            value={formData.openingBalance}
+            onChange={handleChange}
+            placeholder="Opening Balance"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Opening Balance Date</label>
+          <input
+            type="datetime-local"
+            name="openingBalanceDate"
+            value={formData.openingBalanceDate}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Notes</label>
+          <textarea
+            name="notes"
+            value={formData.notes}
+            onChange={handleChange}
+            placeholder="Notes"
+            className="w-full px-3 py-2 border rounded-md"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Business ID No</label>
+          <input
+            type="text"
+            name="businessIdNo"
+            value={formData.businessIdNo}
+            onChange={handleChange}
+            placeholder="Business ID No"
+            className="w-full px-3 py-2 border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Status</label>
+          <input
+            type="checkbox"
+            name="status"
+            checked={formData.status}
+            onChange={(e) =>
+              setFormData({ ...formData, status: e.target.checked })
+            }
+            className="form-checkbox h-5 w-5 text-blue-600"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2">Payment Account</label>
+          <select
+            name="paymentAccount"
+            value={formData.paymentAccount}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-md"
+          >
+            {accounts.map((account, index) => (
+              <option key={index} value={account.id}>
+                {account.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-  <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-    Submit
-  </button>
-</form>
+        <div className="mb-4">
+          <label className="block mb-2">Sub Group ID</label>
+          <select
+            name="subGroupId"
+            value={formData.subGroupId}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-md"
+          >
+            {subGroupAccounts.map((subGroup, index) => (
+              <option key={index} value={subGroup.subGroupAccount.id}>
+                {subGroup.subGroupAccount.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
+      </form>
     </Modal>
   );
 };
