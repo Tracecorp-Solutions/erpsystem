b48baf3f9fd933ae74837bcfbfff91ead9288e4a
unknown <kinyeramo@gmail.com>
2024-05-18 20:05:46 +0300

    completed implementing toggle button

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -35,8 +35,14 @@ const Customer = () => {
   }, []);
 
   const filteredCustomerList = toggleDisabled
-    ? customerList.filter((customer) => customer.status && customer.vendorType === "Customer")
-    : customerList.filter((customer) => customer.vendorType === "Customer");
+    ? customerList.filter(
+        (customer) =>
+          customer.status === true && customer.vendorType === "Customer"
+      )
+    : customerList.filter(
+        (customer) =>
+          customer.status === false && customer.vendorType === "Customer"
+      );
 
   const handleModal = () => {
     setShowModal(true);
@@ -52,19 +58,46 @@ const Customer = () => {
 
   return (
     <div>
-      <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px" }}>
-        <h2 style={{ fontSize: "36px", fontFamily: "Sans-serif", color: "#505050", fontWeight: "600" }}>Customer</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "10px",
+        }}
+      >
+        <h2
+          style={{
+            fontSize: "36px",
+            fontFamily: "Sans-serif",
+            color: "#505050",
+            fontWeight: "600",
+          }}
+        >
+          Customer
+        </h2>
         <Button
           type="button"
-          style={{ background: "#4467a1", fontFamily: "outFit, Sans-serif", color: "#fff", padding: "", borderRadius: "24px", marginTop: "15px" }}
+          style={{
+            background: "#4467a1",
+            fontFamily: "outFit, Sans-serif",
+            color: "#fff",
+            padding: "",
+            borderRadius: "24px",
+            marginTop: "15px",
+          }}
           onClick={handleModal}
         >
           + Create Customer
         </Button>
       </div>
       <CustomerForm showModal={showModal} setShowModal={setShowModal} />
-      <div style={{ background: "#fff", padding: "15px", borderRadius: "24px" }}>
-        <CustomerNavigationbar toggleDisabled={toggleDisabled} setToggleDisabled={setToggleDisabled} />
+      <div
+        style={{ background: "#fff", padding: "15px", borderRadius: "24px" }}
+      >
+        <CustomerNavigationbar
+          toggleDisabled={toggleDisabled}
+          setToggleDisabled={setToggleDisabled}
+        />
         {showFailure && (
           <FailureSlideInCard
             title={messageInfo.title}
@@ -83,26 +116,72 @@ const Customer = () => {
                     type="checkbox"
                     style={{ marginLeft: "10px", marginTop: "15px" }}
                   />
-                  <th scope="col" className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Company Name</th>
-                  <th scope="col" className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                  <th scope="col" className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Phone</th>
-                  <th scope="col" className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Opening Balance</th>
-                  <th scope="col" className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ACTION</th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Company Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Phone
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Opening Balance
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    ACTION
+                  </th>
                 </tr>
               </thead>
               {filteredCustomerList.map((customer) => (
-                <tbody className="bg-white divide-y divide-gray-200" key={customer.id}>
+                <tbody
+                  className="bg-white divide-y divide-gray-200"
+                  key={customer.id}
+                >
                   <tr className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
                     <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
                       <input type="checkbox" />
                     </td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">{customer.companyName}</td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">{customer.email}</td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">{customer.mobile}</td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">{customer.openingBalance}</td>
-                    <div style={{ width: "100px", display: "flex", justifyContent: "center", marginTop: "10px" }}>
+                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                      {customer.companyName}
+                    </td>
+                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                      {customer.email}
+                    </td>
+                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                      {customer.mobile}
+                    </td>
+                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                      {customer.openingBalance}
+                    </td>
+                    <div
+                      style={{
+                        width: "100px",
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "10px",
+                      }}
+                    >
                       <Dropdown overlay={menu} trigger={["click"]}>
-                        <EllipsisVerticalIcon className="h-5 w-5 mt-3" aria-hidden="true" />
+                        <EllipsisVerticalIcon
+                          className="h-5 w-5 mt-3"
+                          aria-hidden="true"
+                        />
                       </Dropdown>
                     </div>
                   </tr>
