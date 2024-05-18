c8f7134c33e5a43afdb169a03de9a6b1de320d5d
unknown <kinyeramo@gmail.com>
2024-05-18 23:16:26 +0300

    completed implementing toggle button

--- src/components/CustomerNavigationbar.js
+++ src/components/CustomerNavigationbar.js
@@ -42,7 +42,7 @@ const CustomerNavigationbar = ({ toggleDisabled, setToggleDisabled, searchQuery,
               fontFamily: "outFit, Sans-serif",
             }}
           >
-            Show non active Customers
+            Show none active Customers
           </p>
             )
           }
