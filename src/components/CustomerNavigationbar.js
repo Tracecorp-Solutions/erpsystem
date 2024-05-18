98e60ad9a3ad1ee2f551a64dbfa9820fd4491ec2
unknown <kinyeramo@gmail.com>
2024-05-18 21:28:07 +0300

    fix the issues

--- src/components/CustomerNavigationbar.js
+++ src/components/CustomerNavigationbar.js
@@ -42,7 +42,7 @@ const CustomerNavigationbar = ({ toggleDisabled, setToggleDisabled, searchQuery,
               fontFamily: "outFit, Sans-serif",
             }}
           >
-            Show Inactive Customers
+            Show non active Customers
           </p>
             )
           }
