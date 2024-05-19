0e59cb2db1da52cbca8fdfa6d20701ecefd69be7
unknown <kinyeramo@gmail.com>
2024-05-19 22:52:18 +0300

    done with adding status

--- src/components/CustomerSidebar.js
+++ src/components/CustomerSidebar.js
@@ -21,7 +21,7 @@ export default function CustomerSidebar({
         closable={false}
         onClose={() => setDrawerVisible(false)}
         visible={drawerVisible}
-        width={500}
+        width={550}
         style={{ height: "100vh", overflow: "auto" }}
       >
         <div className="px- text-end">
@@ -46,6 +46,21 @@ export default function CustomerSidebar({
               >
                 {selectedCustomer.companyName}
               </h2>
+              <div style={{ textAlign: "start" }}>
+                <span style={{
+                  textAlign: "start",
+                  background: "#9EC137",
+                  color: "#fdf5e6",
+                  fontWeight: "500",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "12px",
+                  padding: "5px 10px 5px 10px",
+                  borderRadius: "24px"
+                  }}
+                >
+                {selectedCustomer.status ? "ACTIVE" : "INACTIVE"}
+                </span>
+              </div>{" "}
               <div>
                 <h3
                   style={{
@@ -99,7 +114,7 @@ export default function CustomerSidebar({
                         style={{
                           display: "flex",
                           flexDirection: "column",
-                          marginTop: "20px"
+                          marginTop: "20px",
                         }}
                       >
                         <strong
@@ -184,6 +199,347 @@ export default function CustomerSidebar({
                           }}
                         >
                           {selectedCustomer.phone}
+                        </span>
+                      </div>
+                    </Col>
+                  </Row>
+                </Card>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    color: "#4467a1",
+                    textAlign: "start",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    fontFamily: "outFit, Sans-serif",
+                    marginTop: "20px",
+                  }}
+                >
+                  Contact Information
+                </h3>
+                <Card
+                  style={{
+                    background: "#F6F6F4",
+                  }}
+                >
+                  <Row gutter={[16, 16]}>
+                    <Col span={12}>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          WEBSITE
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.website}
+                        </span>
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          marginTop: "20px",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          ADDRESS
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.companyName}
+                        </span>
+                      </div>
+                    </Col>
+                    <Col span={12}>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          MOBILE
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.mobile}
+                        </span>
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          marginTop: "20px",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          PHONE
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.phone}
+                        </span>
+                      </div>
+                    </Col>
+                  </Row>
+                </Card>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    color: "#4467a1",
+                    textAlign: "start",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    fontFamily: "outFit, Sans-serif",
+                    marginTop: "20px",
+                  }}
+                >
+                  Billing Information
+                </h3>
+                <Card
+                  style={{
+                    background: "#F6F6F4",
+                  }}
+                >
+                  <Row gutter={[16, 16]}>
+                    <Col span={12}>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          OPENING BALANCE
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          ${selectedCustomer.openingBalance}
+                        </span>
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          marginTop: "20px",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          PAYMENT METHOD
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.companyName}
+                        </span>
+                      </div>
+                    </Col>
+                    <Col span={12}>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          BILLING RATE
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.billingRate}
+                        </span>
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          marginTop: "20px",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          ACCOUNT NO
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.accountNo}
+                        </span>
+                      </div>
+                    </Col>
+                  </Row>
+                </Card>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    color: "#4467a1",
+                    textAlign: "start",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    fontFamily: "outFit, Sans-serif",
+                    marginTop: "20px",
+                  }}
+                >
+                  About Customer
+                </h3>
+                <Card
+                  style={{
+                    background: "#F6F6F4",
+                  }}
+                >
+                  <Row gutter={[16, 16]}>
+                    <Col span={12}>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <strong
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "500",
+                            color: "#a1a1a1",
+                            fontSize: "12px",
+                          }}
+                        >
+                          NOTES
+                        </strong>
+                        <span
+                          style={{
+                            textAlign: "start",
+                            fontFamily: "outFit, Sans-serif",
+                            fontWeight: "400",
+                            fontSize: "16px",
+                            color: "#505050",
+                          }}
+                        >
+                          {selectedCustomer.notes}
                         </span>
                       </div>
                     </Col>
