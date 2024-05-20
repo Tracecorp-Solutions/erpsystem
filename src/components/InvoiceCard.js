ec4f923113d0cd9d08d68871732e0ffba69b0ba8
unknown <kinyeramo@gmail.com>
2024-05-20 16:07:33 +0300

    added responsiveness for the card

--- src/components/InvoiceCard.js
+++ src/components/InvoiceCard.js
@@ -2,105 +2,41 @@ import React from 'react';
 import { Card, Col, Row } from 'antd';
 
 const InvoiceCard = () => (
-  <Row gutter={16}>
-    <Col span={6}>
-      <Card bordered={false}>
-        <div style={{
-          display: "flex"
-        }}>
-          <img src="../images/Frame2.svg" width={20} style={{ marginRight: "10px" }} />
-          <strong style={{
-            fontSize: "24px",
-            fontWeight: "600",
-            fontFamily: "outFit, Sans-serif",
-            color: "#505050"
-            }}>
-              $3000
-          </strong>
+  <Row gutter={[16, 16]}>
+    <Col xs={24} sm={12} md={6} lg={6}>
+      <Card bordered={false} className="w-full">
+        <div className="flex items-center">
+          <img src="../images/Frame2.svg" width={20} className="mr-2" alt="Icon" />
+          <strong className="text-lg font-semibold text-gray-800">$3000</strong>
         </div>
-        <p
-         style={{
-            fontSize: "12px",
-            color: "#a1a1a1",
-            fontFamily: "outFit, Sans-serif",
-            fontWeight: "400"
-         }}
-        >OVERDUE AMOUNT</p>
+        <p className="text-xs text-gray-500">OVERDUE AMOUNT</p>
       </Card>
     </Col>
-    <Col span={6}>
-      <Card  bordered={false}>
-      <div style={{
-          display: "flex"
-        }}>
-          <img src="../images/Frame1.svg" width={20} style={{ marginRight: "10px" }} />
-          <strong style={{
-            fontSize: "24px",
-            fontWeight: "600",
-            fontFamily: "outFit, Sans-serif",
-            color: "#505050"
-            }}>
-              $3000
-          </strong>
+    <Col xs={24} sm={12} md={6} lg={6}>
+      <Card bordered={false} className="w-full">
+        <div className="flex items-center">
+          <img src="../images/Frame1.svg" width={20} className="mr-2" alt="Icon" />
+          <strong className="text-lg font-semibold text-gray-800">$3000</strong>
         </div>
-        <p
-         style={{
-            fontSize: "12px",
-            color: "#a1a1a1",
-            fontFamily: "outFit, Sans-serif",
-            fontWeight: "400"
-         }}
-        >OVERDUE AMOUNT</p>
+        <p className="text-xs text-gray-500">OVERDUE AMOUNT</p>
       </Card>
     </Col>
-    <Col span={6}>
-      <Card bordered={false}>
-      <div style={{
-          display: "flex"
-        }}>
-          <img src="../images/Frame5.svg" width={20} style={{ marginRight: "10px" }} />
-          <strong style={{
-            fontSize: "24px",
-            fontWeight: "600",
-            fontFamily: "outFit, Sans-serif",
-            color: "#505050"
-            }}>
-              $3000
-          </strong>
+    <Col xs={24} sm={12} md={6} lg={6}>
+      <Card bordered={false} className="w-full">
+        <div className="flex items-center">
+          <img src="../images/Frame5.svg" width={20} className="mr-2" alt="Icon" />
+          <strong className="text-lg font-semibold text-gray-800">$3000</strong>
         </div>
-        <p
-         style={{
-            fontSize: "12px",
-            color: "#a1a1a1",
-            fontFamily: "outFit, Sans-serif",
-            fontWeight: "400"
-         }}
-        >OVERDUE AMOUNT</p>
+        <p className="text-xs text-gray-500">OVERDUE AMOUNT</p>
       </Card>
     </Col>
-    <Col span={6}>
-      <Card  bordered={false}>
-      <div style={{
-          display: "flex"
-        }}>
-          <img src="../images/Frame6.svg" width={20} style={{ marginRight: "10px" }} />
-          <strong style={{
-            fontSize: "24px",
-            fontWeight: "600",
-            fontFamily: "outFit, Sans-serif",
-            color: "#505050"
-            }}>
-              $3000
-          </strong>
+    <Col xs={24} sm={12} md={6} lg={6}>
+      <Card bordered={false} className="w-full">
+        <div className="flex items-center">
+          <img src="../images/Frame6.svg" width={20} className="mr-2" alt="Icon" />
+          <strong className="text-lg font-semibold text-gray-800">$3000</strong>
         </div>
-        <p
-         style={{
-            fontSize: "12px",
-            color: "#a1a1a1",
-            fontFamily: "outFit, Sans-serif",
-            fontWeight: "400"
-         }}
-        >OVERDUE AMOUNT</p>
+        <p className="text-xs text-gray-500">OVERDUE AMOUNT</p>
       </Card>
     </Col>
   </Row>
