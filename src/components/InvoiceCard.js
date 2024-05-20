59b85c02510f5c2a0a721203cd83b9161d7183d9
unknown <kinyeramo@gmail.com>
2024-05-20 16:05:10 +0300

    added images for the card

--- src/components/InvoiceCard.js
+++ src/components/InvoiceCard.js
@@ -5,22 +5,102 @@ const InvoiceCard = () => (
   <Row gutter={16}>
     <Col span={6}>
       <Card bordered={false}>
-        Card content
+        <div style={{
+          display: "flex"
+        }}>
+          <img src="../images/Frame2.svg" width={20} style={{ marginRight: "10px" }} />
+          <strong style={{
+            fontSize: "24px",
+            fontWeight: "600",
+            fontFamily: "outFit, Sans-serif",
+            color: "#505050"
+            }}>
+              $3000
+          </strong>
+        </div>
+        <p
+         style={{
+            fontSize: "12px",
+            color: "#a1a1a1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "400"
+         }}
+        >OVERDUE AMOUNT</p>
       </Card>
     </Col>
     <Col span={6}>
       <Card  bordered={false}>
-        Card content
+      <div style={{
+          display: "flex"
+        }}>
+          <img src="../images/Frame1.svg" width={20} style={{ marginRight: "10px" }} />
+          <strong style={{
+            fontSize: "24px",
+            fontWeight: "600",
+            fontFamily: "outFit, Sans-serif",
+            color: "#505050"
+            }}>
+              $3000
+          </strong>
+        </div>
+        <p
+         style={{
+            fontSize: "12px",
+            color: "#a1a1a1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "400"
+         }}
+        >OVERDUE AMOUNT</p>
       </Card>
     </Col>
     <Col span={6}>
       <Card bordered={false}>
-        Card content
+      <div style={{
+          display: "flex"
+        }}>
+          <img src="../images/Frame5.svg" width={20} style={{ marginRight: "10px" }} />
+          <strong style={{
+            fontSize: "24px",
+            fontWeight: "600",
+            fontFamily: "outFit, Sans-serif",
+            color: "#505050"
+            }}>
+              $3000
+          </strong>
+        </div>
+        <p
+         style={{
+            fontSize: "12px",
+            color: "#a1a1a1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "400"
+         }}
+        >OVERDUE AMOUNT</p>
       </Card>
     </Col>
     <Col span={6}>
       <Card  bordered={false}>
-        Card content
+      <div style={{
+          display: "flex"
+        }}>
+          <img src="../images/Frame6.svg" width={20} style={{ marginRight: "10px" }} />
+          <strong style={{
+            fontSize: "24px",
+            fontWeight: "600",
+            fontFamily: "outFit, Sans-serif",
+            color: "#505050"
+            }}>
+              $3000
+          </strong>
+        </div>
+        <p
+         style={{
+            fontSize: "12px",
+            color: "#a1a1a1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "400"
+         }}
+        >OVERDUE AMOUNT</p>
       </Card>
     </Col>
   </Row>
