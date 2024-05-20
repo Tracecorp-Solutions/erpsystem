fd972dab7ae9f2a59f309bc02f5a06ad4c7cea63
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-05-20 17:00:20 +0300

    ADD FILTERS

--- src/components/BillsNavigationBar.js
+++ src/components/BillsNavigationBar.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Switch, Input } from "antd";
-import { FilterOutlined, SearchOutlined } from "@ant-design/icons";
+import {
+  FilterOutlined,
+  SearchOutlined,
+  FolderOutlined,
+  CalendarOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
 import "../styles/components/AccountNavigationFilter.css";
 
 const BillsNavigationbar = ({
@@ -11,71 +17,76 @@ const BillsNavigationbar = ({
 }) => {
   return (
     <div className="flex flex-col md:flex-row md:items-center md:mb-4 w-full">
-      <div>
-        <p
-          className="text-gray-600 text-sm mb-1"
-          style={{ fontFamily: "outFit, Sans-serif" }}
-        >
-          Vendors
-        </p>
-        <Input
-          placeholder="Bills"
-          prefix={<SearchOutlined />}
-          className="mb-2 md:mb-0 md:mr-4 w-full md:w-auto"
-          style={{ borderRadius: "24px", padding: "10px" }}
-          value={searchQuery}
-          onChange={handleSearch}
-        />
+      <div
+        style={{ display: "flex", justifyContent: "space-between", flex: 1 }}
+      >
+        <div style={{ marginBottom: "16px",  }}>
+          <p
+            className="text-gray-600 text-sm mb-1"
+            style={{ fontFamily: "outFit, Sans-serif" }}
+          >
+            To
+          </p>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              border: "1px solid #d9d9d9",
+              borderRadius: "24px",
+              padding: "3px",
+            }}
+          >
+            <Input
+              placeholder="Choose bill date"
+              value={searchQuery}
+              onChange={handleSearch}
+              style={{
+                border: "none",
+                boxShadow: "none",
+                flex: 1,
+                marginRight: "4px",
+              }}
+              bordered={false}
+            />
+            <CalendarOutlined style={{ fontSize: "16px", color: "#bfbfbf" }} />
+          </div>
+        </div>
       </div>
-      <div>
-       
-        <p
-          className="text-gray-600 text-sm mb-1"
-          style={{ fontFamily: "outFit, Sans-serif" }}
-        >
-          status
-        </p>
-        <Input
-          placeholder="Bills"
-          prefix={<SearchOutlined />}
-          className="mb-2 md:mb-0 md:mr-4 w-full md:w-auto"
-          style={{ borderRadius: "24px", padding: "10px" }}
-          value={searchQuery}
-          onChange={handleSearch}
-        />
+      <div
+        style={{ display: "flex", justifyContent: "end", flex: 1,  alignItems: "center", }}
+      >
+        <div style={{ marginBottom: "16px",    alignItems: "center", }}>
+          <p
+            className="text-gray-600 text-sm mb-1"
+            style={{ fontFamily: "outFit, Sans-serif" }}
+          >
+            To
+          </p>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              border: "1px solid #d9d9d9",
+              borderRadius: "24px",
+              padding: "3px",
+            }}
+          >
+            <Input
+              placeholder=""
+              value={searchQuery}
+              onChange={handleSearch}
+              style={{
+                border: "none",
+                boxShadow: "none",
+                flex: 1,
+                marginRight: "4px",
+              }}
+              bordered={false}
+            />
+            <CalendarOutlined style={{ fontSize: "16px", color: "#bfbfbf" }} />
+          </div>
+        </div>
       </div>
-      <div>
-      <p
-          className="text-gray-600 text-sm mb-1"
-          style={{ fontFamily: "outFit, Sans-serif" }}
-        >
-          from
-        </p>
-      <Input
-        placeholder="Bills"
-        prefix={<SearchOutlined />}
-        className="mb-2 md:mb-0 md:mr-4 w-full md:w-auto"
-        style={{ borderRadius: "24px", padding: "10px" }}
-        value={searchQuery}
-        onChange={handleSearch}
-      />
-      </div>
-      
-     <div>
-     <p
-          className="text-gray-600 text-sm mb-1"
-          style={{ fontFamily: "outFit, Sans-serif" }}
-        >
-          to
-        </p> 
-     <Input
-        placeholder="Filter"
-        prefix={<FilterOutlined style={{ fontSize: "25px" }} />}
-        className="mb-2 md:mb-0 md:mr-4 md:w-auto w-full lg:w-22"
-        style={{ borderRadius: "24px", padding: "10px" }}
-      />
-     </div>
-      
     </div>
   );
 };
