81e8c74f12d56cfaa3a480f0dc1ff365546eb5a4
unknown <kinyeramo@gmail.com>
2024-05-21 22:18:13 +0300

    added table for displaying data

--- src/components/InvoiceSidebar.js
+++ src/components/InvoiceSidebar.js
@@ -1,12 +1,40 @@
 import React, { useState } from "react";
-import { Drawer, Modal, Card, Row, Col } from "antd";
+import { Drawer, Modal, Card, Row, Col, Table } from "antd";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-export default function InvoiceSidebar({
-  setDrawerVisible,
-  drawerVisible,
-}) {
+export default function InvoiceSidebar({ setDrawerVisible, drawerVisible }) {
   const [modalVisible, setModalVisible] = useState(false);
+
+  const tableData = [
+    {
+      key: "1",
+      description: "Product 1",
+      amount: 100,
+    },
+    {
+      key: "2",
+      description: "Product 2",
+      amount: 200,
+    },
+    {
+      key: "3",
+      description: "Product 3",
+      amount: 300,
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+      key: "amount",
+    },
+  ];
 
   return (
     <>
@@ -52,35 +80,142 @@ export default function InvoiceSidebar({
           </p>
         </div>
 
-        <Row gutter={[16, 16]}>
+        <Row gutter={[16, 16]} style={{ marginTop: "15px" }}>
           <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%" }}>
-              <h4>BILL TO</h4>
-              <p>Acme Graphic Design</p>
-              <p>123 Main Street, Cityville, Uganda</p>
+            <Card style={{ width: "100%", background: "#f6f6f4" }}>
+              <h4
+                style={{
+                  color: "#a1a1a1",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  fontFamily: "outFit, Sans-serif",
+                }}
+              >
+                BILL TO
+              </h4>
+              <p
+                style={{
+                  color: "#505050",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                Acme Graphic Design
+              </p>
+              <p
+                style={{
+                  color: "#505050",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                123 Main Street, Cityville, Uganda
+              </p>
             </Card>
           </Col>
           <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%" }}>
-              <h4>BILL FROM</h4>
-              <p>Tracecorp solutions LTD</p>
-              <p>123 Main Street, Cityville, Uganda</p>
+            <Card style={{ width: "100%", background: "#f6f6f4" }}>
+              <h4
+                style={{
+                  color: "#a1a1a1",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  fontFamily: "outFit, Sans-serif",
+                }}
+              >
+                BILL FROM
+              </h4>
+              <p
+                style={{
+                  color: "#505050",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                Tracecorp solutions LTD
+              </p>
+              <p
+                style={{
+                  color: "#505050",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                123 Main Street, Cityville, Uganda
+              </p>
             </Card>
           </Col>
           <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%" }}>
-              <h4>ISSUED ON</h4>
-              <p>09/12/2024</p>
+            <Card style={{ width: "100%", background: "#f6f6f4" }}>
+              <h4
+                style={{
+                  color: "#a1a1a1",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  fontFamily: "outFit, Sans-serif",
+                }}
+              >
+                ISSUED ON
+              </h4>
+              <p
+                style={{
+                  color: "#505050",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                09/12/2024
+              </p>
             </Card>
           </Col>
           <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%" }}>
-              <h4>DUE ON</h4>
-              <p>09/12/2024</p>
+            <Card style={{ width: "100%", background: "#f6f6f4" }}>
+              <h4
+                style={{
+                  color: "#a1a1a1",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                  fontFamily: "outFit, Sans-serif",
+                }}
+              >
+                DUE ON
+              </h4>
+              <p
+                style={{
+                  color: "#505050",
+                  fontFamily: "outFit, Sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                09/12/2024
+              </p>
             </Card>
           </Col>
         </Row>
 
+        <h2
+          style={{
+            color: "#505050",
+            fontWeight: "600",
+            fontFamily: "outFit, Sans-serif",
+            fontSize: "24px",
+            marginTop: "30px",
+          }}
+        >
+          Invoice Items
+        </h2>
+        <Table
+          dataSource={tableData}
+          columns={columns}
+          pagination={false}
+          style={{ marginTop: "20px" }}
+        />
         <Modal
           visible={modalVisible}
           onCancel={() => setModalVisible(false)}
