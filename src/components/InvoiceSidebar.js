e385698d4f61194ff48ebc9bd75b0fbf2d5c92bc
unknown <kinyeramo@gmail.com>
2024-05-21 21:57:02 +0300

    added four card

--- src/components/InvoiceSidebar.js
+++ src/components/InvoiceSidebar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Drawer, Modal, Card, Row, Col } from "antd";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
@@ -28,8 +28,59 @@ export default function InvoiceSidebar({
             <span className="sr-only">Close panel</span>
             <XMarkIcon className="h-10 w-10" aria-hidden="true" />
           </button>
-         
+          <h2
+            style={{
+              fontSize: "36px",
+              textAlign: "start",
+              color: "#505050",
+              fontFamily: "outFit, Sans-serif",
+            }}
+          >
+            Invoice 5678
+          </h2>
+          <p
+            style={{
+              background: "#F6F6F4",
+              padding: "5px 10px 5px 10px",
+              textAlign: "start",
+              borderRadius: "24px",
+              width: "75px",
+              fontSize: "12px",
+            }}
+          >
+            UNPAID
+          </p>
         </div>
+
+        <Row gutter={[16, 16]}>
+          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+            <Card style={{ width: "100%" }}>
+              <h4>BILL TO</h4>
+              <p>Acme Graphic Design</p>
+              <p>123 Main Street, Cityville, Uganda</p>
+            </Card>
+          </Col>
+          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+            <Card style={{ width: "100%" }}>
+              <h4>BILL FROM</h4>
+              <p>Tracecorp solutions LTD</p>
+              <p>123 Main Street, Cityville, Uganda</p>
+            </Card>
+          </Col>
+          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+            <Card style={{ width: "100%" }}>
+              <h4>ISSUED ON</h4>
+              <p>09/12/2024</p>
+            </Card>
+          </Col>
+          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+            <Card style={{ width: "100%" }}>
+              <h4>DUE ON</h4>
+              <p>09/12/2024</p>
+            </Card>
+          </Col>
+        </Row>
+
         <Modal
           visible={modalVisible}
           onCancel={() => setModalVisible(false)}
