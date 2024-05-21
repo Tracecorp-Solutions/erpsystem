2eb24c1cf2a815102e9b83a1c5554190f9048b55
unknown <kinyeramo@gmail.com>
2024-05-21 13:44:03 +0300

    fix issues for description

--- src/components/InvoiceForm.js
+++ src/components/InvoiceForm.js
@@ -224,34 +224,26 @@ const InvoiceForm = () => {
               <thead className="bg-gray-50">
                 <tr>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Invoice Number
+                    Description
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Invoice Date
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Due Date
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Customer
+                    Amount
                   </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
                 {items.map((item, index) => (
                   <tr key={index}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.invoiceNumber}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.invoiceDate}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.dueDate}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {item.customer}
-                    </td>
+                    {item.billTranItems.map((tranItem, index) => (
+                      <td key={index} className="px-6 py-4 whitespace-nowrap">
+                        {tranItem.description}
+                      </td>
+                    ))}
+                    {item.billTranItems.map((tranItem, index) => (
+                      <td key={index} className="px-6 py-4 whitespace-nowrap">
+                        {tranItem.amount}
+                      </td>
+                    ))}
                   </tr>
                 ))}
               </tbody>
