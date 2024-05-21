3181693e88df87f7f2136e6735834db0712a2cb6
unknown <kinyeramo@gmail.com>
2024-05-22 00:16:54 +0300

    added download options

--- src/components/InvoiceSidebar.js
+++ src/components/InvoiceSidebar.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Drawer, Modal, Card, Row, Col, Table, Button } from "antd";
+import { Drawer, Card, Row, Col, Table, Button } from "antd";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import jsPDF from "jspdf";
+import html2canvas from "html2canvas"; // Import html2canvas library
 
 export default function InvoiceSidebar({ setDrawerVisible, drawerVisible }) {
   const [modalVisible, setModalVisible] = useState(false);
@@ -51,15 +52,12 @@ export default function InvoiceSidebar({ setDrawerVisible, drawerVisible }) {
     const doc = new jsPDF();
 
     doc.setFontSize(16);
-    doc.text("Invoice", 20, 20);
 
-    let y = 30;
-    data.forEach((item, index) => {
-      doc.text(`${index + 1}. ${item.description} - $${item.amount}`, 20, y);
-      y += 10;
+    html2canvas(document.querySelector("#invoiceContent")).then((canvas) => {
+      const contentDataURL = canvas.toDataURL("image/png");
+      doc.addImage(contentDataURL, "PNG", 0, 0, 210, 297);
+      doc.save("invoice.pdf");
     });
-
-    return doc.output("blob");
   };
 
   return (
@@ -72,7 +70,11 @@ export default function InvoiceSidebar({ setDrawerVisible, drawerVisible }) {
         width={550}
         style={{ height: "100vh", overflow: "auto" }}
       >
-        <div className="px- text-end">
+        <div
+          style={{
+            textAlign: "end",
+          }}
+        >
           <button
             type="button"
             onClick={() => setDrawerVisible(false)}
@@ -82,182 +84,194 @@ export default function InvoiceSidebar({ setDrawerVisible, drawerVisible }) {
             <span className="sr-only">Close panel</span>
             <XMarkIcon className="h-10 w-10" aria-hidden="true" />
           </button>
+        </div>
+        <div id="invoiceContent">
+          <div className="px- text-end" style={{
+            marginLeft: "20px",
+            marginRight: "20px"
+          }}>
+            <h2
+              style={{
+                fontSize: "36px",
+                textAlign: "start",
+                color: "#505050",
+                fontFamily: "outFit, Sans-serif",
+              }}
+            >
+              Invoice 5678
+            </h2>
+            <p
+              style={{
+                // background: "#F6F6F4",
+                padding: "5px 10px 5px 10px",
+                textAlign: "start",
+                borderRadius: "24px",
+                width: "75px",
+                fontSize: "12px",
+              }}
+            >
+              UNPAID
+            </p>
+          </div>
+
+          <Row gutter={[16, 16]} style={{ marginTop: "15px",  marginLeft: "20px",
+            marginRight: "20px" }}>
+            <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+              <Card style={{ width: "100%", background: "#f6f6f4" }}>
+                <h4
+                  style={{
+                    color: "#a1a1a1",
+                    fontSize: "12px",
+                    fontWeight: "500",
+                    fontFamily: "outFit, Sans-serif",
+                  }}
+                >
+                  BILL TO
+                </h4>
+                <p
+                  style={{
+                    color: "#505050",
+                    fontFamily: "outFit, Sans-serif",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                  }}
+                >
+                  Acme Graphic Design
+                </p>
+                <p
+                  style={{
+                    color: "#505050",
+                    fontFamily: "outFit, Sans-serif",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                  }}
+                >
+                  123 Main Street, Cityville, Uganda
+                </p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+              <Card style={{ width: "100%", background: "#f6f6f4" }}>
+                <h4
+                  style={{
+                    color: "#a1a1a1",
+                    fontSize: "12px",
+                    fontWeight: "500",
+                    fontFamily: "outFit, Sans-serif",
+                  }}
+                >
+                  BILL FROM
+                </h4>
+                <p
+                  style={{
+                    color: "#505050",
+                    fontFamily: "outFit, Sans-serif",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                  }}
+                >
+                  Tracecorp solutions LTD
+                </p>
+                <p
+                  style={{
+                    color: "#505050",
+                    fontFamily: "outFit, Sans-serif",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                  }}
+                >
+                  123 Main Street, Cityville, Uganda
+                </p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+              <Card style={{ width: "100%", background: "#f6f6f4" }}>
+                <h4
+                  style={{
+                    color: "#a1a1a1",
+                    fontSize: "12px",
+                    fontWeight: "500",
+                    fontFamily: "outFit, Sans-serif",
+                  }}
+                >
+                  ISSUED ON
+                </h4>
+                <p
+                  style={{
+                    color: "#505050",
+                    fontFamily: "outFit, Sans-serif",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                  }}
+                >
+                  09/12/2024
+                </p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+              <Card style={{ width: "100%", background: "#f6f6f4" }}>
+                <h4
+                  style={{
+                    color: "#a1a1a1",
+                    fontSize: "12px",
+                    fontWeight: "500",
+                    fontFamily: "outFit, Sans-serif",
+                  }}
+                >
+                  DUE ON
+                </h4>
+                <p
+                  style={{
+                    color: "#505050",
+                    fontFamily: "outFit, Sans-serif",
+                    fontSize: "16px",
+                    fontWeight: "400",
+                  }}
+                >
+                  09/12/2024
+                </p>
+              </Card>
+            </Col>
+          </Row>
+
           <h2
             style={{
-              fontSize: "36px",
-              textAlign: "start",
               color: "#505050",
+              fontWeight: "600",
               fontFamily: "outFit, Sans-serif",
+              fontSize: "24px",
+              marginTop: "30px",
+              marginLeft: "25px",
             }}
           >
-            Invoice 5678
+            Invoice Items
           </h2>
-          <p
-            style={{
-              background: "#F6F6F4",
-              padding: "5px 10px 5px 10px",
-              textAlign: "start",
-              borderRadius: "24px",
-              width: "75px",
-              fontSize: "12px",
-            }}
-          >
-            UNPAID
-          </p>
+          <Table
+            dataSource={tableData}
+            columns={columns}
+            pagination={false}
+            style={{ marginTop: "20px",  marginLeft: "20px",
+            marginRight: "20px" }}
+          />
         </div>
 
-        <Row gutter={[16, 16]} style={{ marginTop: "15px" }}>
-          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%", background: "#f6f6f4" }}>
-              <h4
-                style={{
-                  color: "#a1a1a1",
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  fontFamily: "outFit, Sans-serif",
-                }}
-              >
-                BILL TO
-              </h4>
-              <p
-                style={{
-                  color: "#505050",
-                  fontFamily: "outFit, Sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                }}
-              >
-                Acme Graphic Design
-              </p>
-              <p
-                style={{
-                  color: "#505050",
-                  fontFamily: "outFit, Sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                }}
-              >
-                123 Main Street, Cityville, Uganda
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%", background: "#f6f6f4" }}>
-              <h4
-                style={{
-                  color: "#a1a1a1",
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  fontFamily: "outFit, Sans-serif",
-                }}
-              >
-                BILL FROM
-              </h4>
-              <p
-                style={{
-                  color: "#505050",
-                  fontFamily: "outFit, Sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                }}
-              >
-                Tracecorp solutions LTD
-              </p>
-              <p
-                style={{
-                  color: "#505050",
-                  fontFamily: "outFit, Sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                }}
-              >
-                123 Main Street, Cityville, Uganda
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%", background: "#f6f6f4" }}>
-              <h4
-                style={{
-                  color: "#a1a1a1",
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  fontFamily: "outFit, Sans-serif",
-                }}
-              >
-                ISSUED ON
-              </h4>
-              <p
-                style={{
-                  color: "#505050",
-                  fontFamily: "outFit, Sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                }}
-              >
-                09/12/2024
-              </p>
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-            <Card style={{ width: "100%", background: "#f6f6f4" }}>
-              <h4
-                style={{
-                  color: "#a1a1a1",
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  fontFamily: "outFit, Sans-serif",
-                }}
-              >
-                DUE ON
-              </h4>
-              <p
-                style={{
-                  color: "#505050",
-                  fontFamily: "outFit, Sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                }}
-              >
-                09/12/2024
-              </p>
-            </Card>
-          </Col>
-        </Row>
-
-        <h2
-          style={{
-            color: "#505050",
-            fontWeight: "600",
-            fontFamily: "outFit, Sans-serif",
-            fontSize: "24px",
-            marginTop: "30px",
-          }}
-        >
-          Invoice Items
-        </h2>
-        <Table
-          dataSource={tableData}
-          columns={columns}
-          pagination={false}
-          style={{ marginTop: "20px" }}
-        />
         <div
           style={{
             marginTop: "30px",
             display: "flex",
             justifyContent: "space-between",
+            marginLeft: "20px",
+            marginRight: "20px"
           }}
         >
           <Button
             type="default"
-            onClick={() => setDrawerVisible(false)}
             style={{
               width: "150px",
               borderRadius: "24px",
             }}
           >
-            Cancel
+            Send Invoice
           </Button>
           <Button
             type="button"
@@ -273,11 +287,6 @@ export default function InvoiceSidebar({ setDrawerVisible, drawerVisible }) {
             Download Invoice
           </Button>
         </div>
-        <Modal
-          visible={modalVisible}
-          onCancel={() => setModalVisible(false)}
-          footer={null}
-        ></Modal>
       </Drawer>
     </>
   );
