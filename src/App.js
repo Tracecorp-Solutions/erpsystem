6b1a68201b63f5219de7258992a6ed5d3edd8876
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-05-24 17:30:59 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Routers } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './components/Header';
-
+import Signup from './pages/authentication/Signup';
+import Reset from './pages/authentication/Reset';
+import Verify from './pages/authentication/Verify';
+import Login from './pages/authentication/Login';
+import Forgot from './pages/authentication/Forgot';
 
 function App() {
-
   return (
     <div className="min-h-full">
       <div className="py-">
-        <Routers>
-         <Main />
-        </Routers>
+        <Router>
+          <Main />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/reset" element={<Reset />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="/forgot" element={<Forgot />} />
+          </Routes>
+        </Router>
       </div>
     </div>
+
   );
 }
 
 export default App;
+
