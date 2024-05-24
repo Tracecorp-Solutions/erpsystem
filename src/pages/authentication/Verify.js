dbe90316c66420f93f1907f89cdaab8fc6ab393b
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-05-24 17:44:10 +0300

    reset

---
+++ src/pages/authentication/Verify.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import About from '../../components/About';
+
+function Verify({ onSubmit }) {
+    const [formData, setFormData] = useState({
+      otp: ''
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value
+      });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      onSubmit(formData);
+    };
+  
+    return (
+      <div className="flex">
+        <div className="form-side">
+          <div className="form-content">
+            <div className="form-intro">
+              <span className="greeting">
+                <h2>Verify Account</h2>
+                <img src="/img/wave.png" alt="signup" />
+              </span>
+              <p>A one-time password (OTP) has been sent to your email address. Enter the code to verify your account.</p>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <div className="label-desc">
+                  <label>One-time Password</label>
+                </div>
+                <input
+                  type="text"
+                  name="otp"
+                  placeholder="Enter OTP"
+                  value={formData.otp}
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit" className="create-btn">Verify Account</button>
+            </form>
+          </div>
+        </div>
+        <About />
+      </div>
+    );
+
+}
+
+export default Verify;
