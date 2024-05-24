7ef90bab14132ffb16c8ac0ad27cd00688aab143
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-05-24 17:35:24 +0300

    add

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+function About() {
+    return (
+        <div className="img-side">
+            <div className="img-content">
+            <div className="img-intro">
+                <h2>TraceAccounting</h2>
+                <p>We are excited to help you streamline your invoicing process and efficiently manage your finances. Let’s get started.</p>
+            </div>
+            <div className="img-wrapper">
+                <img src="/img/dashboard.png" alt="signup" />
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
