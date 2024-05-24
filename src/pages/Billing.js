58320632e45fbeb45a04af9e867a516c25354a72
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-05-24 10:47:11 +0300

    add mark as paid

--- src/pages/Billing.js
+++ src/pages/Billing.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Dropdown, Menu, Button, Pagination } from "antd";
+import { Dropdown, Menu, Button, Pagination, Modal } from "antd";
 import { EyeOutlined, EditOutlined } from "@ant-design/icons";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import axios from "axios";
@@ -19,13 +19,16 @@ const Bills = () => {
   const [toggleDisabled, setToggleDisabled] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5);
+  const [selectBillId, setSelectedBillId] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
   const [showSuccess, setShowSuccess] = useState(false);
   const [drawerVisible, setDrawerVisible] = useState(false);
   const [total, setTotalAmount] = useState(0);
   const [paidTotalAmount, setPaidTotalAmount] = useState(0);
   const [unpaidTotalAmount, setUnpaidTotalAmount] = useState(0);
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
+  console.log("mark as paid form data", selectBillId);
   console.log("bills details", Bills);
 
   const navigate = useNavigate();
@@ -38,29 +41,29 @@ const Bills = () => {
         );
         setBills(response.data);
         const total = response.data.reduce(
-          (acc, inv) =>
-            acc + inv.billTranItems.reduce((acc, item) => acc + item.amount, 0),
+          (acc, bill) =>
+            acc + bill.billTranItems.reduce((acc, item) => acc + item.amount, 0),
           0
         );
         setTotalAmount(total);
 
         // Calculate total amount for paid invoices
         const paidTotal = response.data
-          .filter((inv) => inv.status === "Paid")
+          .filter((bill) => bill.status === "Paid")
           .reduce(
-            (acc, inv) =>
+            (acc, bill) =>
               acc +
-              inv.billTranItems.reduce((acc, item) => acc + item.amount, 0),
+              bill.billTranItems.reduce((acc, item) => acc + item.amount, 0),
             0
           );
         setPaidTotalAmount(paidTotal);
 
         const unpaidTotal = response.data
-        .filter((inv) => inv.status !== "Paid")
+        .filter((bill) => bill.status !== "Paid")
         .reduce(
-          (acc, inv) =>
+          (acc, bill) =>
             acc +
-            inv.billTranItems.reduce((acc, item) => acc + item.amount, 0),
+            bill.billTranItems.reduce((acc, item) => acc + item.amount, 0),
           0
         );
       setUnpaidTotalAmount(unpaidTotal);
@@ -84,6 +87,38 @@ const Bills = () => {
   const handleViewClick = () => {
     // Open the sidebar drawer when "View" is clicked
     setDrawerVisible(true);
+  };
+
+  const handleMarkAsPaid = async (id) => {
+    console.log(id);
+    setSelectedBillId(id);
+    setShowConfirmationModal(true);
+
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/GetBillById/${id}`
+      );
+      console.log("Fetched Invoice Data:", response.data);
+    } catch (error) {
+      console.error("Error fetching invoice data:", error);
+      // Optionally, you can display an error message to the user
+    }
+  };
+
+  const handleConfirmation = async () => {
+    const id = selectBillId;
+
+    setShowConfirmationModal(false);
+
+    try {
+      await axios.get(`${process.env.REACT_APP_API_URL}/PayBill/${id}`);
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/GetAllBills`
+      );
+      setBills(response.data);
+    } catch (error) {
+      console.error("Error marking bill as paid:", error);
+    }
   };
 
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -235,13 +270,13 @@ const Bills = () => {
                                 <Menu.Item key="1" onClick={handleViewClick}>
                                   <span>View Invoice</span>
                                 </Menu.Item>
-                                {/* <Menu.Item
+                                <Menu.Item
                                   key="2"
-                                  onClick={() => handleEditInvoice(bill.id)}
+                                  // onClick={() => handleEditInvoice(bill.id)}
                                 >
                                   Edit View
                                 </Menu.Item>
-                                {inv.status !== "Paid" && (
+                                {bill.status !== "Paid" && (
                                   <Menu.Item
                                     key="3"
                                     onClick={() => handleMarkAsPaid(bill.id)}
@@ -251,7 +286,7 @@ const Bills = () => {
                                 )}
                                 <Menu.Item key="4">
                                   Send payment reminder
-                                </Menu.Item> */}
+                                </Menu.Item>
                               </Menu>
                             }
                             trigger={["click"]}
@@ -270,10 +305,32 @@ const Bills = () => {
           </div>
         </div>
       </div>
-      {/* <InvoiceSidebar
-        drawerVisible={drawerVisible}
-        setDrawerVisible={setDrawerVisible}
-      /> */}
+      <Modal
+        title="Mark Invoice as Paid"
+        visible={showConfirmationModal}
+        onOk={handleConfirmation}
+        onCancel={() => setShowConfirmationModal(false)}
+        okButtonProps={{
+          // Style props for OK button
+          style: {
+            backgroundColor: "#4467a1",
+            color: "#fff",
+            borderRadius: "4px",
+            borderColor: "#4467a1",
+            marginRight: "8px",
+          },
+        }}
+        cancelButtonProps={{
+          style: {
+            backgroundColor: "#fff",
+            color: "#4467a1",
+            borderRadius: "4px",
+            borderColor: "#4467a1",
+          },
+        }}
+      >
+        <p>Are you sure you want to mark this invoice as paid?</p>
+      </Modal>
     </div>
   );
 };
