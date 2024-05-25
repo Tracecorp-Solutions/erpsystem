2c8bbabbf3b933d789144d8e308f9752be7ef949
unknown <kinyeramo@gmail.com>
2024-05-26 00:47:02 +0300

    added button for saving profile

--- src/pages/onboarding/Profile.js
+++ src/pages/onboarding/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import RegisterCompany from "./RegisterCompany";
+import { CheckCircleIcon } from '@heroicons/react/20/solid';
 import ProfileCompletionForm from "./ProfileCompletionForm ";
 
 const Profile = () => {
@@ -11,6 +12,40 @@ const Profile = () => {
 
   return (
     <div>
+      <div className="px-4 py-12 sm:px-6 lg:px-8">
+        <nav className="flex justify-center" aria-label="Progress">
+          <ol role="list" className="space-y-6">
+            <li>
+              <span className="flex items-start">
+                <span className="relative flex h-5 w-5 flex-shrink-0 items-center justify-center">
+                  {currentStep >= 1 ? (
+                    <CheckCircleIcon className="h-full w-full text-indigo-600" aria-hidden="true" />
+                  ) : (
+                    <span className="h-full w-full bg-gray-300" aria-hidden="true" />
+                  )}
+                </span>
+                <span className="ml-3 text-sm font-medium text-gray-500">
+                  Personal Information
+                </span>
+              </span>
+            </li>
+            <li>
+              <span className="flex items-start">
+                <span className="relative flex h-5 w-5 flex-shrink-0 items-center justify-center">
+                  {currentStep >= 2 ? (
+                    <CheckCircleIcon className="h-full w-full text-indigo-600" aria-hidden="true" />
+                  ) : (
+                    <span className="h-full w-full bg-gray-300" aria-hidden="true" />
+                  )}
+                </span>
+                <span className="ml-3 text-sm font-medium text-gray-500">
+                  Company Information
+                </span>
+              </span>
+            </li>
+          </ol>
+        </nav>
+      </div>
       {currentStep === 1 && (
         <ProfileCompletionForm moveToNextStep={moveToNextStep} />
       )}
