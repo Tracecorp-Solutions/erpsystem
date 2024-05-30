e63ad9986883c64cd35b06d24b9a93a7b9302952
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-05-30 19:59:31 +0300

    adding signup form on the refactor

--- src/shared/auth/Login.js
+++ src/shared/auth/Login.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import About from "../../components/About"; // Importing the About component
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -7,121 +6,108 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [feedback, setFeedback] = useState("");
-  const [showPassword, setShowPassword] = useState(false); // Define showPassword state
-  const [loading, setloading] = useState(false); // set loading state
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const navigate = useNavigate(); // useNavigate hook to programmatically navigate
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    //set state to load as you call the api
-    setloading(true);
+    setLoading(true);
     try {
-      //call the authenticate user api
       const response = await axios.post(
         `${process.env.REACT_APP_API_URL}/AuthenticateUser`,
         { username, password }
       );
       const token = response.data;
-      //store token in the session
       sessionStorage.setItem("token", token);
-      //navigate profile to process user
       navigate("/profile");
     } catch (error) {
-      setFeedback(`Error logging in ${error}`);
+      setFeedback(`${error.response.data}`);
     } finally {
-      setloading(false);
+      setLoading(false);
     }
   };
 
   const handleCheckboxChange = () => {
-    // Define handleCheckboxChange function
     setShowPassword((prevState) => !prevState);
   };
 
   return (
-    <div className="flex" style={{ height: "95vh" }}>
-      <div className="form-side">
-        <div className="form-content">
-          <div className="form-intro">
-            <span className="greeting">
-              <h2
-                style={{
-                  fontFamily: "sans-serif",
-                  marginTop: "20px",
-                  marginBottom: "0",
-                }}
-              >
-                Hello
-              </h2>
-              <img src="/img/wave.png" alt="login" />
-            </span>
-          </div>
-          <form onSubmit={handleLogin}>
-            {feedback && (
-              <div
-                className={`message ${
-                  feedback.startsWith("Error")
-                    ? "error-message"
-                    : "success-message"
-                }`}
-              >
-                {feedback}
-              </div>
-            )}
-            <div className="form-group">
-              <div className="label-desc">
-                <label htmlFor="username">Username:</label>
-              </div>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Enter your username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="input-field"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <div className="label-desc">
-                <label htmlFor="password">Password:</label>
-              </div>
-              <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="input-field"
-                required
-              />
-            </div>
-            <div style={{ marginBottom: "5px" }}>
-              <input
-                type="checkbox"
-                id="showPassword"
-                checked={showPassword}
-                onChange={handleCheckboxChange}
-                style={{ marginBottom: "2px" }}
-              />
-              <label htmlFor="showPassword" style={{ marginBottom: "2px" }}>
-                Show Password
-              </label>
-            </div>
-            <button type="submit" className="create-btn" disabled={loading}>
-              {loading ? "Authenticating..." : "Login"}
-            </button>
-          </form>
-          <p>
-            Don’t have an account? <Link to="/signup">Register here</Link>
-          </p>
-        </div>
+    <>
+      <div className="form-intro">
+        <span className="greeting">
+          <h2
+            style={{
+              fontFamily: "sans-serif",
+              marginTop: "20px",
+              marginBottom: "0",
+            }}
+          >
+            Hello
+          </h2>
+          <img src="/img/wave.png" alt="login" />
+        </span>
       </div>
-      <About />
-    </div>
+      <form onSubmit={handleLogin}>
+        {feedback && (
+          <div
+            className={`message ${feedback.startsWith("Error") ? "error-message" : "success-message"}`}
+          >
+            {feedback}
+          </div>
+        )}
+        <div className="form-group">
+          <div className="label-desc">
+            <label htmlFor="username">Username:</label>
+          </div>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input-field"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <div className="label-desc">
+            <label htmlFor="password">Password:</label>
+          </div>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input-field"
+            required
+          />
+        </div>
+        <div style={{ marginBottom: "5px" }}>
+          <input
+            type="checkbox"
+            id="showPassword"
+            checked={showPassword}
+            onChange={handleCheckboxChange}
+            style={{ marginBottom: "2px" }}
+          />
+          <label htmlFor="showPassword" style={{ marginBottom: "2px" }}>
+            Show Password
+          </label>
+        </div>
+        <button type="submit" className="create-btn" disabled={loading}>
+          {loading ? "Authenticating..." : "Login"}
+        </button>
+      </form>
+      <p>
+        Don’t have an account? <Link to={{ pathname: "/layout", state: { screen: "signup" } }}>Register here</Link>
+
+      </p>
+    </>
   );
 };
 
--- src/shared/auth/shared/Layout.js
+++ src/shared/auth/shared/Layout.js
@@ -1,26 +1,35 @@
+import { useLocation } from "react-router-dom";
 import Login from "../../auth/Login";
+import Signup from "../../auth/Signup";
 
 function Layout() {
-    return (
-        <div className="flex"> {/* Set height of the page */}
-        <Login/>
-        {/* Signup right image section */}
-        <div className="img-side">
-            <div className="img-content">
-                <div className="img-intro">
-                    <h2>TraceAccounting</h2>
-                    <p>We are excited to help you streamline your invoicing process and efficiently manage your finances. Let’s get started.</p>
-                </div>
-                <div className="img-wrapper">
-                    <img src="/img/dashboard.png" alt="signup" />
-                </div>
-            </div>
+  const location = useLocation();
+  const { state } = location;
+  const screen = state?.screen || 'login';
+
+  console.log("Current screen:", screen); // Log the current screen
+
+  return (
+    <div className="flex">
+      <div className="form-side">
+        <div className="form-content">
+          {screen === 'signup' ? <Signup /> : <Login />}
         </div>
-        {/* end of signup right image section */}
+      </div>
+
+      <div className="img-side">
+        <div className="img-content">
+          <div className="img-intro">
+            <h2>TraceAccounting</h2>
+            <p>We are excited to help you streamline your invoicing process and efficiently manage your finances. Let’s get started.</p>
+          </div>
+          <div className="img-wrapper">
+            <img src="/img/dashboard.png" alt="signup" />
+          </div>
         </div>
-        
-        
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
