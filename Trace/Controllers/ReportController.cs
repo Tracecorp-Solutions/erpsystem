79fad79d2fb2aa0b035c57c7fd2596bc72ebd00c
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-06-06 11:50:42 +0300

    published files

---
+++ Trace/Controllers/ReportController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Trace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportRepository _reportRepository;
+        public ReportController(IReportRepository reportRepository)
+        {
+            _reportRepository = reportRepository;
+        }
+
+        [HttpGet("account-statement")]
+        public async Task<IActionResult> GetAccountStatement(int accountId, DateOnly startDate, DateOnly endDate)
+        {
+            try
+            {
+                var accountStatement = await _reportRepository.GetAccountStatement(accountId, startDate, endDate);
+                return Ok(accountStatement);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+    }
+}
