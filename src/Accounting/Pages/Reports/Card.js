970d1388b2aa9591a865fe83943423dc6a7db11b
unknown <kinyeramo@gmail.com>
2024-06-06 14:33:27 +0300

    added the style for cards

--- src/Accounting/Pages/Reports/Card.js
+++ src/Accounting/Pages/Reports/Card.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const Cards = () => {
   return (
-    <div className="flex" style={{ justifyContent: "start", border: "2px solid red" }}>
-      <div style={{ border: "2px solid red", marginRight: "10px" }}>
-        <div className="bg-white border border-gray-300 rounded shadow p-4" style={{ width: '150px' }}>
-          Card content 1
+    <div className="flex flex-wrap justify-start p-2">
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 px-2">
+        <div
+          className="bg-white rounded p-4 text-center"
+          style={{
+            color: "#A1A1A1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "600",
+          }}
+        >
+          Account Statements
         </div>
       </div>
-      <div style={{ border: "2px solid red", marginRight: "10px", marginLeft: "10px" }}>
-        <div className="bg-white border border-gray-300 rounded shadow p-4" style={{ width: '150px' }}>
-          Card content 2
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 px-2">
+        <div
+          className="bg-white rounded p-4 text-center"
+          style={{
+            color: "#A1A1A1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "600",
+          }}
+        >
+          Balance Sheets
         </div>
       </div>
-      <div style={{ marginRight: "10px", marginLeft: "10px" }}>
-        <div className="bg-white border border-gray-300 rounded shadow p-4" style={{ width: '150px' }}>
-          Card content 3
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 px-2">
+        <div
+          className="bg-white rounded p-4 text-center"
+          style={{
+            color: "#A1A1A1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "600",
+          }}
+        >
+          Trial Balance
         </div>
       </div>
-      <div style={{ marginRight: "10px" }}>
-        <div className="bg-white border border-gray-300 rounded shadow p-4" style={{ width: '150px' }}>
-          Card content 4
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 px-2">
+        <div
+          className="bg-white rounded p-4 text-center"
+          style={{
+            color: "#A1A1A1",
+            fontFamily: "outFit, Sans-serif",
+            fontWeight: "600",
+          }}
+        >
+          User Activity
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Cards;
