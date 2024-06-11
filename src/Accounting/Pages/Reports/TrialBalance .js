f86b1b952626216efa04e4b561632f7d0e050043
unknown <kinyeramo@gmail.com>
2024-06-11 16:16:05 +0300

    fix the table

--- src/Accounting/Pages/Reports/TrialBalance .js
+++ src/Accounting/Pages/Reports/TrialBalance .js
@@ -3,6 +3,7 @@ import { Table, Pagination } from "antd";
 import TrialBalanceFilter from "./TrialBalanceFilter";
 import ErrorMessageCard from "../../components/Shared/ErrorMessageCard";
 import EmptyData from "../../components/Shared/EmptyData";
+import axios from "axios";
 
 const TrialBalance = () => {
   const [data, setData] = useState([]);
@@ -12,26 +13,20 @@ const TrialBalance = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
-    // Simulating fetching data
-    const fetchData = async () => {
-      try {
-        // Fetch your data here
-        // Example:
-        // const response = await axios.get(`${process.env.REACT_APP_API_URL}/trial_balance`);
-        // setData(response.data);
-        setData([
-          { key: "1", account: "Account 1", description: "Description 1", credit: 1000, debit: 500 },
-          { key: "2", account: "Account 2", description: "Description 2", credit: 1500, debit: 2000 },
-          { key: "3", account: "Account 3", description: "Description 3", credit: 2000, debit: 1000 },
-        ]);
-      } catch (error) {
-        setShowFailure(true);
-        console.error("Error fetching data:", error);
-      }
-    };
-
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "http://3.216.182.63:8095/TestApi/GetTrialBalance"
+      );
+      setData(response.data);
+    } catch (error) {
+      setShowFailure(true);
+      console.error("Error fetching data:", error);
+    }
+  };
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
@@ -42,7 +37,6 @@ const TrialBalance = () => {
 
   const filteredData = data.filter(
     (item) =>
-      item.account.toLowerCase().includes(searchQuery.toLowerCase()) ||
       item.description.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
@@ -55,24 +49,29 @@ const TrialBalance = () => {
 
   const columns = [
     {
-      title: "Account",
-      dataIndex: "account",
-      key: "account",
-    },
-    {
       title: "Description",
       dataIndex: "description",
       key: "description",
     },
     {
-      title: "Credit",
-      dataIndex: "credit",
-      key: "credit",
+      title: "Debit Account",
+      dataIndex: "debitAccount",
+      key: "debitAccount",
     },
     {
-      title: "Debit",
-      dataIndex: "debit",
-      key: "debit",
+      title: "Credit Account",
+      dataIndex: "creditAccount",
+      key: "creditAccount",
+    },
+    {
+      title: "Debit Amount",
+      dataIndex: "debitAmount",
+      key: "debitAmount",
+    },
+    {
+      title: "Credit Amount",
+      dataIndex: "creditAmount",
+      key: "creditAmount",
     },
   ];
 
