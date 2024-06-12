78c323fae87542176adc61877106fcb355e31d96
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-06-12 14:00:13 +0300

    addition of new connection endpoint and billing module configuration endpoints

---
+++ Trace/Controllers/NewConnectionController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Models;
+using Core.Repositories.Billing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewConnectionController : ControllerBase
+    {
+        private readonly INewConnectionRepository _newconnectionRepository;
+
+        public NewConnectionController(INewConnectionRepository newconnectionRepository) 
+        {
+            _newconnectionRepository = newconnectionRepository;
+        }
+
+        [HttpPost("/NewApplication")]
+        public IActionResult NewApplication([FromBody] Application application) 
+        {
+            try
+            {
+                _newconnectionRepository.RegisterNewCustomer(application);
+                return Ok("ApplicationRegisteredSuccessfully");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+    }
+}
