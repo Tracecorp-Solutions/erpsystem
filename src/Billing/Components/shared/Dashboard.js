7aee1cbfee43abef3cba4f35170668c231b3c355
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-06-18 08:38:13 +0300

    Add nearest land mark

--- src/Billing/Components/shared/Dashboard.js
+++ src/Billing/Components/shared/Dashboard.js
@@ -731,7 +731,12 @@ function NewConnection() {
                               type="text"
                               name="nearestLandmark"
                               value={formData.nearestLandMark}
-                              onChange={handleChange}
+                              onChange={(e) =>
+                                setFormData({
+                                  ...formData,
+                                  nearestLandMark: e.target.value,
+                                })
+                              }
                               className="border border-gray-300 rounded-md px-3 py-2"
                             />
                           </div>
