10b6c947c53e5f1739c0ebcff5064ed121851d71
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-06-20 19:28:35 +0300

    published files

--- Trace/obj/Release/net8.0/Trace.AssemblyInfo.cs
+++ Trace/obj/Release/net8.0/Trace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9a018c54797fc04455c508056cbf1c34301f97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553d6cf9c68849f1a56ca7161700baea9c997230")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
