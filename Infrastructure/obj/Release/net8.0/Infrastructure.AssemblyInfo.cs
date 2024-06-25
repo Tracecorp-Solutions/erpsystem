a1fcafb1bfd838bac4be8cb0846084fe9a1fc0b7
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-06-25 14:37:53 +0300

    published files with docket initiation migration

--- Infrastructure/obj/Release/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Release/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78c5478bd0e3a382bfa5ef5b681987ffcace7b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41d757121373a7d6e99fc4fd73678ed6de504a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Release/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Release/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78c5478bd0e3a382bfa5ef5b681987ffcace7b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41d757121373a7d6e99fc4fd73678ed6de504a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
