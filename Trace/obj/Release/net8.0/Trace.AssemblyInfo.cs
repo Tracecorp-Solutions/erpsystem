29a963739a4df6a466720c0316b9670309419a71
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-02 18:27:21 +0300

    published code

--- Trace/obj/Release/net8.0/Trace.AssemblyInfo.cs
+++ Trace/obj/Release/net8.0/Trace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2313f301cdceee68e45d87cc47a71e6bab162dbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8119cc4b7cb82463fa1125621080daad9b5a4c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
