51cafaabaf03f2b6709473c0c6a0a073b4edd1f5
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-02 07:11:43 +0300

    published code

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c08692ca9f85e5216f929df7b9f98887fbcaab74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36347e6fa443386713547d1c32d7d20a09fcf64e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Trace/obj/Debug/net8.0/Trace.AssemblyInfo.cs
+++ Trace/obj/Debug/net8.0/Trace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62780ca545260051cdf3a0893c3774c2ee144dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36347e6fa443386713547d1c32d7d20a09fcf64e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
