0d0c8d1ac2ed4158bc6efacb0e04d1543aa0ae51
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-06 22:21:39 +0300

    addition of migration

--- Services/obj/Release/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Release/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a763f7e56538fd177418bbb4bfe18f43a6eac33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3594a70220e1bac84ff3e46746bee8d4f93f7780")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
