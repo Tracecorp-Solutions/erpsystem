127399ba04259d36554857ecfd121acf319d05b6
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-08 13:39:40 +0300

    optimised build

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadd41e56a215c375f1d1b22b3645aa31bbf9c38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5896d709037bda043d43f02e26992ae732513ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
