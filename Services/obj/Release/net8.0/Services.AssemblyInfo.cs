e70d4e9d3673dd986ae9a25945d904898ad41f8d
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-15 15:42:48 +0300

    published files

--- Services/obj/Release/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Release/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdafb08ad83090e186016d9f60677f7ec7a17d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3610e20b4a6f795f2ac62c73363fd7984d52ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
