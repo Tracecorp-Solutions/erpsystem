ac77b54870e371c99e0ed93e8de94826eabac6c1
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-16 09:44:32 +0300

    published code

--- Core/obj/Release/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Release/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25760cb5ad48df6df348986c77c5e1a01e3d46d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f20a74c394dc61ff53e8112844ca2c78f2e3094")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
