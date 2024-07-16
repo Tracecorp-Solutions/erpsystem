7fa716cd1e7a28767a95c9b62714f636ed041672
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-07-16 11:10:24 +0300

    add the crm topnav

---
+++ src/CRM/Pages/Layout/Crm.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import SideNav from "../../Shared/SideNav";
+import TopNav from "../../Shared/TopNav";
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import CrmDashboard from "../Dashboard/CrmDashboard";
+
+const Crm = () => {
+  const location = useLocation();
+  const { state } = location;
+  const screen = state?.screen || "crm";
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (sessionStorage.getItem("userid") == null) {
+      navigate('/');
+      return;
+    }
+  }, [navigate]);
+
+  return (
+    <div className="flex flex-col md:flex-row w-full">
+      <SideNav />
+      <main className="w-full w-9/12">
+        <TopNav />
+        {/* Page header */}
+
+        <div className="w-full flex justify-center">
+          <div className="main-content bg-stone-100 pb-6 rounded-t-3xl">
+            <div className="content px-4 sm:px-6 lg:px-8 group-container">
+              {screen === "crm-dashboard" && <CrmDashboard />}
+             
+        
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  
+  );
+};
+
+export default Crm;
---
+++ src/CRM/Shared/TopNav.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Plus, BellDot, ChevronDown, Menu } from "lucide-react";
+
+function TopNav() {
+  const user = {
+    name: "Nakitto Catherine",
+    imageUrl: "./img/profile-pic.png",
+    imageSize: 40,
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobileNavVisible, setIsMobileNavVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleNav = () => {
+    setIsMobileNavVisible(!isMobileNavVisible);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div
+      className="topnav"
+      style={{
+        position: "sticky",
+        right: 0,
+        width: "80%",
+        zIndex: 1000,
+        alignContent: "flex-end",
+      }}
+    >
+      <ul>
+        {isMobile ? null : (
+          <li>
+            <div
+              className="button-container"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "10px",
+              }}
+            >
+              <button className="btn" onClick={toggleDropdown}>
+                <Plus />
+                <span>New Entry</span>
+                <ChevronDown />
+              </button>
+            </div>
+
+            {isOpen && (
+              <div
+                className="dropdown-content"
+                style={{
+                  position: "absolute",
+                  backgroundColor: "#f9f9f9",
+                  minWidth: "160px",
+                  zIndex: 1001,
+                  top: "calc(100% + 5px)",
+                }}
+              >
+                <Link
+                  to="/vendors"
+                  style={{ color: "#007bff", padding: "10px", display: "block" }}
+                >
+                  Vendors
+                </Link>
+                <Link
+                  to="/customer"
+                  style={{ color: "#007bff", padding: "10px", display: "block" }}
+                >
+                  Customers
+                </Link>
+                <Link
+                  to="/account"
+                  style={{ color: "#007bff", padding: "10px", display: "block" }}
+                >
+                  New Account
+                </Link>
+                <Link
+                  to="/billing"
+                  style={{ color: "#007bff", padding: "10px", display: "block" }}
+                >
+                  Billing
+                </Link>
+                <Link
+                  to="/invoice"
+                  style={{ color: "#007bff", padding: "10px", display: "block" }}
+                >
+                  Invoice
+                </Link>
+              </div>
+            )}
+          </li>
+        )}
+
+        <li>
+          <Link to="/" className="notif">
+            <BellDot />
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="profile">
+            <img
+              className="avatar"
+              src={user.imageUrl}
+              alt={"Photo of " + user.name}
+              style={{
+                width: user.imageSize,
+                height: user.imageSize,
+              }}
+            />
+            <span className="user-name">{user.name}</span>
+            <ChevronDown />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default TopNav;
