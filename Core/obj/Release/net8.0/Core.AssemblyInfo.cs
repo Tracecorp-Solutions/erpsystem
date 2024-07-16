393aca7eec1dd78c87df11ed115123baa82aa0bb
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-16 20:01:58 +0300

    published code

--- Core/obj/Release/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Release/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b4d025e48c6a047db24e04f8d4eaa30ebe802f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0230670302c96b1f1c545a86ffc2e65d3d26a6c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Trace/obj/Debug/net8.0/Trace.AssemblyInfo.cs
+++ Trace/obj/Debug/net8.0/Trace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c5b3156c0b796c800865ec00c48ddb8f205ccd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0230670302c96b1f1c545a86ffc2e65d3d26a6c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
