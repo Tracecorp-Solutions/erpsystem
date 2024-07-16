020ef11e46516e47a4fc8005ede402890de4b921
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-07-16 11:09:42 +0300

    add the crm dashboard

---
+++ src/CRM/Pages/Dashboard/CrmDashboard.js
@@ -0,0 +1,20 @@
+
+
+const CrmDashboard = () => {
+
+    return (
+        <>
+            <div className="flex flex-col items-start px-6 w-full max-md:px-5 max-md:max-w-full">
+                <time className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                    3rd May, 2024
+                </time>
+                <h1 className="text-4xl font-semibold leading-[57.6px] text-neutral-600">
+                    Dashboard
+                </h1>
+            </div>
+            
+        </>
+    );
+};
+
+export default CrmDashboard;
