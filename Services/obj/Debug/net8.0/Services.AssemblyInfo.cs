a9f91ead4c44f1a74de1f2f8eb0b4090a585a54f
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-18 15:47:21 +0300

    Update AssemblyInfo to new commit hash
    This commit updates the AssemblyInformationalVersionAttribute across multiple projects to the new commit hash `b9ab8ec7b1de613826a70c117bea4a864b84a01c`, ensuring synchronization of versioning across the codebase. Additionally, cache identifiers in `.cache` files have been updated to reflect changes in build inputs or dependencies, enhancing the build process's accuracy and reliability.

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0217092aff05b63785a878c5408525cdd917b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9ab8ec7b1de613826a70c117bea4a864b84a01c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
