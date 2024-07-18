a0701a9e72f495704c0b92f35f13ab60c235523f
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-18 09:45:48 +0300

    Update assembly info and add Priority model
    - Updated `AssemblyInformationalVersionAttribute` in Core.AssemblyInfo.cs to new commit hash.
    - Updated Core.AssemblyInfoInputs.cache with new hash value.
    - Added new `Priority` class in Core.Models.CRM for CRM system prioritization.

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b83ba364e53ecc0b616ad953cd5c88fbe070cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6789d96be189055f2519730b0b20d44c43845873")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
