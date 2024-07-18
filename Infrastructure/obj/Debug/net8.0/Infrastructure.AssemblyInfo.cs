b9ab8ec7b1de613826a70c117bea4a864b84a01c
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-18 15:43:59 +0300

    Update assembly informational versions
    Updated AssemblyInformationalVersionAttribute in Infrastructure.AssemblyInfo.cs, Services.AssemblyInfo.cs, and Trace.AssemblyInfo.cs to new commit hash fb0217092aff05b63785a878c5408525cdd917b1. Cache files for each assembly also updated to reflect new input hashes. Other assembly attributes remain unchanged.

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074721fd8085c8760a598e14985d3f9939f379e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0217092aff05b63785a878c5408525cdd917b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1562364cdb74d7dfe79408f9372a803a72d0f656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0217092aff05b63785a878c5408525cdd917b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
