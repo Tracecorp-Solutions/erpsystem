1c3112bbe3ae54eca7a1ae8794db6977bd380690
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2024-07-22 09:31:25 +0300

    addition of migration

---
+++ Infrastructure/Migrations/20240722062858_AdditionOfNewFieldsOnEscalationMatrix.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionOfNewFieldsOnEscalationMatrix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentLevel",
+                table: "EscalationMatrices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailTemplate",
+                table: "EscalationMatrices",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NotificationType",
+                table: "EscalationMatrices",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmentLevel",
+                table: "EscalationMatrices");
+
+            migrationBuilder.DropColumn(
+                name: "EmailTemplate",
+                table: "EscalationMatrices");
+
+            migrationBuilder.DropColumn(
+                name: "NotificationType",
+                table: "EscalationMatrices");
+        }
+    }
+}
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b6911f452e30dd319eefa6fb659c942c4654759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a9ec31b15dbe6442c249c624e0ff9c7c24ccd80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
