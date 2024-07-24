f0fbf29964725247ebbad4421dd31c72fb8ee7f4
unknown <kinyeramo@gmail.com>
2024-07-24 18:35:09 +0300

    completed escalation tickets

--- src/CRM/Pages/Ticket/UpdateTicket.js
+++ src/CRM/Pages/Ticket/UpdateTicket.js
@@ -15,7 +15,9 @@ const UpdateTicket = () => {
   useEffect(() => {
     const fetchTicketDetails = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/GetTicketById/${ticketId}`);
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/GetTicketById/${ticketId}`
+        );
         console.log("API response:", response.data);
         setTicketDetails(response.data);
         setLoading(false);
@@ -24,7 +26,7 @@ const UpdateTicket = () => {
         setLoading(false);
       }
     };
-  
+
     if (ticketId) {
       fetchTicketDetails();
     }
@@ -93,7 +95,6 @@ const UpdateTicket = () => {
     },
   ];
 
-
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -113,7 +114,6 @@ const UpdateTicket = () => {
     dateResolved: moment(item.dateResolved).format("YYYY/MM/DD"),
   }));
 
-
   return (
     <div className="flex flex-col flex-wrap justify-center content-start py-6 rounded-3xl bg-stone-100">
       <div className="flex gap-2 justify-between items-center px-6 w-full max-md:flex-wrap max-md:px-5 max-md:max-w-full">
@@ -124,7 +124,9 @@ const UpdateTicket = () => {
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/c154d8841f585cbf3e0cb5a7536d530af74e2f624e3ed71fad0bd8d20294a6a1?apiKey=0d95acea82cc4b259a61e827c24c5c6c&&apiKey=0d95acea82cc4b259a61e827c24c5c6c"
             className="shrink-0 self-stretch my-auto w-6 aspect-square"
           />
-          <div className="self-stretch px-4 py-1 bg-white rounded-2xl">{ticketDetails.ticket.id}</div>
+          <div className="self-stretch px-4 py-1 bg-white rounded-2xl">
+            {ticketDetails.ticket.id}
+          </div>
         </div>
         <div className="flex flex-col justify-center self-stretch">
           <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
@@ -145,7 +147,9 @@ const UpdateTicket = () => {
       </div>
       <div className="flex flex-col self-center px-6 pt-4 pb-6 mt-6 w-full bg-white rounded-3xl max-w-[1088px] max-md:px-5 max-md:max-w-full">
         <div className="flex gap-5 justify-between w-full font-semibold max-md:flex-wrap max-md:max-w-full">
-          <div className="text-3xl capitalize text-neutral-600">{ticketDetails.ticket.complaintSubject}</div>
+          <div className="text-3xl capitalize text-neutral-600">
+            {ticketDetails.ticket.complaintSubject}
+          </div>
           <div className="flex gap-2 my-auto text-base leading-6 text-slate-500">
             <img
               loading="lazy"
@@ -156,58 +160,140 @@ const UpdateTicket = () => {
           </div>
         </div>
         <div className="flex flex-col px-4 pt-2 pb-4 mt-6 rounded-lg bg-stone-100 max-md:max-w-full">
-          <div className="text-base font-semibold leading-6 text-neutral-600 max-md:max-w-full">Ticket Details</div>
+          <div className="text-base font-semibold leading-6 text-neutral-600 max-md:max-w-full">
+            Ticket Details
+          </div>
           <div className="shrink-0 mt-4 h-px border border-solid bg-neutral-500 bg-opacity-10 border-neutral-500 border-opacity-10 w-full" />
           <div className="flex justify-between w-full gap-2 mt-4 max-md:flex-wrap">
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">created on</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">{ticketDetails.ticket.creationDate}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                created on
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.creationDate}
+              </div>
             </div>
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">ticket category</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">{ticketDetails.ticket.ticketCategory.name}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                ticket category
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.ticketCategory.name}
+              </div>
             </div>
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">ticket source</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">{ticketDetails.ticket.ticketSource}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                ticket source
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.ticketSource}
+              </div>
             </div>
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">Ticket priority</div>
-              <div className="mt-2 text-base leading-6 text-orange-400">{ticketDetails.ticket.priority.priorityName}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                Ticket priority
+              </div>
+              <div className="mt-2 text-base leading-6 text-orange-400">
+                {ticketDetails.ticket.priority.priorityName}
+              </div>
             </div>
           </div>
           <div className="flex gap-2 self-start mt-6 max-md:flex-wrap">
             <div className="flex flex-col justify-center whitespace-nowrap">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">department</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                department
+              </div>
               <div className="mt-2 text-base leading-6 text-neutral-600">
                 {ticketDetails.ticket.escalationMatrix.department.name}
               </div>
             </div>
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">ticket subcategory</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">Plumbing</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                ticket subcategory
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                Plumbing
+              </div>
             </div>
           </div>
-          <div className="mt-6 text-xs font-medium tracking-wide uppercase text-neutral-400 max-md:max-w-full">description</div>
+          <div className="mt-6 text-xs font-medium tracking-wide uppercase text-neutral-400 max-md:max-w-full">
+            description
+          </div>
           <div className="mt-2 text-base leading-7 text-neutral-600 max-md:max-w-full">
             {ticketDetails.ticket.escalationMatrix.department.description}
           </div>
         </div>
-        <div className="flex flex-col px-4 pt-2 pb-4 mt-6 rounded-lg bg-stone-100 w-full">
-          <div className="text-base font-semibold leading-6 text-neutral-600 max-md:max-w-full">Customer Details</div>
-          <div className="shrink-0 mt-4 h-px border border-solid bg-neutral-500 bg-opacity-10 border-neutral-500 border-opacity-10 w-full" />
-          <div className="flex justify-between gap-2 mt-4 max-md:flex-wrap">
+        <div className="flex flex-col px-4 pt-2 pb-4 rounded-lg bg-stone-100">
+          <div className="text-base font-semibold leading-6 text-neutral-600 max-md:max-w-full">
+            Customer Details
+          </div>
+          <div className="shrink-0 mt-4 h-px border border-solid bg-neutral-500 bg-opacity-10 border-neutral-500 border-opacity-10 max-md:max-w-full" />
+          <div className="flex w-full justify-between mt-4 max-md:flex-wrap">
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">customer type</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">{ticketDetails.ticket.customerType}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                customer type
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.customerType}
+              </div>
             </div>
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">customer reference</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">{ticketDetails.ticket.customerType}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                customer reference
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.customerRef}
+              </div>
             </div>
             <div className="flex flex-col justify-center">
-              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">FULL NAME</div>
-              <div className="mt-2 text-base leading-6 text-neutral-600">{ticketDetails.ticket.customerType}</div>
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                FULL NAME
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.customerName}
+              </div>
+            </div>
+            <div className="flex flex-col justify-center">
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                phone number
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.phoneNumber}
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between w-full mt-6 max-md:flex-wrap">
+            <div className="flex flex-col justify-center">
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                address
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.address}
+              </div>
+            </div>
+            <div className="flex flex-col justify-center whitespace-nowrap">
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                area
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.operationArea.name}
+              </div>
+            </div>
+            <div className="flex flex-col justify-center whitespace-nowrap">
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                branch
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.branch.name}
+              </div>
+            </div>
+            <div className="flex flex-col justify-center">
+              <div className="text-xs font-medium tracking-wide uppercase text-neutral-400">
+                territory
+              </div>
+              <div className="mt-2 text-base leading-6 text-neutral-600">
+                {ticketDetails.ticket.territory.name}
+              </div>
             </div>
           </div>
         </div>
