c3304056e326e93fd5c49a6f52568dd7a9186a84
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-08-08 07:43:53 +0300

    Update the server error

--- src/Accounting/Pages/Customers/Customer.js
+++ src/Accounting/Pages/Customers/Customer.js
@@ -48,8 +48,6 @@ const Customer = () => {
   const [drawerVisible, setDrawerVisible] = useState(false);
   const [customerDetails, setCustomerDetails] = useState(null);
 
-  console.log("customer details", customerDetails);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -57,12 +55,8 @@ const Customer = () => {
           `${process.env.REACT_APP_API_URL}/GetAllVendors`
         );
         setCustomerList(response.data);
+        setShowFailure(false);
       } catch (error) {
-        setShowFailure(true);
-        setMessageInfo({
-          title: "Server Error!",
-          message: "Failed to fetch customer details.",
-        });
         console.error("Error fetching data:", error);
       }
     };
@@ -191,207 +185,205 @@ const Customer = () => {
   return (
     <>
       <div className="content">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: "10px",
-        }}
-      >
-        {showSuccess && (
-          <SuccessMessageCard
-            title={messageInfo.title}
-            message={messageInfo.message}
-            onClose={() => setShowSuccess(false)}
-          />
-        )}
-        <h2
-          style={{
-            fontSize: "36px",
-            fontFamily: "Sans-serif",
-            color: "#505050",
-            fontWeight: "600",
-          }}
-        >
-          Customer
-        </h2>
-        <Button
-          type="button"
-          style={{
-            background: "#4467a1",
-            fontFamily: "outFit, Sans-serif",
-            color: "#fff",
-            padding: "",
-            borderRadius: "24px",
-            marginTop: "15px",
-          }}
-          onClick={handleModal}
-        >
-          + Create Customer
-        </Button>
-      </div>
-      <CustomerForm
-        handleSearch={handleSearch}
-        handleSubmit={handleSubmit}
-        setFormData={setFormData}
-        formData={formData}
-        showModal={showModal}
-        setShowModal={setShowModal}
-      />
-      <CustomerSidebar
-        drawerVisible={drawerVisible}
-        setDrawerVisible={setDrawerVisible}
-        customerDetails={customerDetails}
-      />
-      <div
-        style={{ background: "#fff", padding: "15px", borderRadius: "24px" }}
-      >
-        <CustomerNavigationbar
-          toggleDisabled={toggleDisabled}
-          setToggleDisabled={setToggleDisabled}
-          searchQuery={searchQuery}
-          handleSearch={handleSearch}
-        />
-        {showFailure && (
-          <ErrorMessageCard
-            title={messageInfo.title}
-            message={messageInfo.message}
-            onClose={() => setShowFailure(false)}
-          />
-        )}
-        {currentItems.length === 0 ? (
-          <EmptyData />
-        ) : (
-          <div style={{ overflowY: "auto" }}>
-            <table className="table-auto min-w-full divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr style={{ borderRadius: "50px" }}>
-                  <input
-                    type="checkbox"
-                    style={{ marginLeft: "10px", marginTop: "15px" }}
-                  />
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Company Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Email
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Phone
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Opening Balance
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    ACTION
-                  </th>
-                </tr>
-              </thead>
-              {currentItems.map((customer) => (
-                <tbody
-                  className="bg-white divide-y divide-gray-200"
-                  key={customer.id}
-                >
-                  <tr className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
-                      <input type="checkbox" />
-                    </td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
-                      {customer.companyName}
-                    </td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
-                      {customer.email}
-                    </td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
-                      {customer.mobile}
-                    </td>
-                    <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
-                      {customer.openingBalance}
-                    </td>
-                    <div
-                      style={{
-                        width: "100px",
-                        display: "flex",
-                        justifyContent: "center",
-                        marginTop: "10px",
-                      }}
-                    >
-                      <Dropdown
-                        overlay={
-                          <Menu style={{ width: "250px" }}>
-                            <Menu.Item
-                              key="1"
-                              onClick={() =>
-                                handleViewCustomerDetails(customer.id)
-                              }
-                            >
-                              <EyeOutlined style={{ marginRight: "5px" }} />
-                              <span>View</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">Action 2</Menu.Item>
-                            <Menu.Item key="3">Action 3</Menu.Item>
-                          </Menu>
-                        }
-                        trigger={["click"]}
-                      >
-                        <EllipsisVerticalIcon
-                          className="h-5 w-5 mt-3"
-                          aria-hidden="true"
-                        />
-                      </Dropdown>
-                    </div>
-                  </tr>
-                </tbody>
-              ))}
-            </table>
-          </div>
-        )}
         <div
           style={{
             display: "flex",
             justifyContent: "space-between",
-            marginRight: "40px",
-            marginTop: "10px",
+            marginBottom: "10px",
           }}
         >
-          <div
+          {showSuccess && (
+            <SuccessMessageCard
+              title={messageInfo.title}
+              message={messageInfo.message}
+              onClose={() => setShowSuccess(false)}
+            />
+          )}
+          <h2
             style={{
-              textAlign: "center",
-              marginTop: "15px",
-              fontSize: "12px",
-              color: "#a1a1a1",
+              fontSize: "36px",
+              fontFamily: "Sans-serif",
+              color: "#505050",
+              fontWeight: "600",
             }}
           >
-            Showing {indexOfFirstItem + 1} - {indexOfLastItem} of{" "}
-            {filteredCustomerList.length} results
-          </div>
-          <Pagination
-            current={currentPage}
-            total={filteredCustomerList.length}
-            pageSize={itemsPerPage}
-            onChange={paginate}
+            Customer
+          </h2>
+          <Button
+            type="button"
+            style={{
+              background: "#4467a1",
+              fontFamily: "outFit, Sans-serif",
+              color: "#fff",
+              padding: "",
+              borderRadius: "24px",
+              marginTop: "15px",
+            }}
+            onClick={handleModal}
+          >
+            + Create Customer
+          </Button>
+        </div>
+        <CustomerForm
+          handleSearch={handleSearch}
+          handleSubmit={handleSubmit}
+          setFormData={setFormData}
+          formData={formData}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+        <CustomerSidebar
+          drawerVisible={drawerVisible}
+          setDrawerVisible={setDrawerVisible}
+          customerDetails={customerDetails}
+        />
+        <div
+          style={{ background: "#fff", padding: "15px", borderRadius: "24px" }}
+        >
+          <CustomerNavigationbar
+            toggleDisabled={toggleDisabled}
+            setToggleDisabled={setToggleDisabled}
+            searchQuery={searchQuery}
+            handleSearch={handleSearch}
           />
+          {showFailure && (
+            <ErrorMessageCard
+              title={messageInfo.title}
+              message={messageInfo.message}
+              onClose={() => setShowFailure(false)}
+            />
+          )}
+          {filteredCustomerList.length === 0 ? (
+            <EmptyData />
+          ) : (
+            <div style={{ overflowY: "auto" }}>
+              <table className="table-auto min-w-full divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr style={{ borderRadius: "50px" }}>
+                    <input
+                      type="checkbox"
+                      style={{ marginLeft: "10px", marginTop: "15px" }}
+                    />
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Company Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Phone
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Opening Balance
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      ACTION
+                    </th>
+                  </tr>
+                </thead>
+                {currentItems.map((customer) => (
+                  <tbody
+                    className="bg-white divide-y divide-gray-200"
+                    key={customer.id}
+                  >
+                    <tr className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                      <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                        <input type="checkbox" />
+                      </td>
+                      <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm  text-gray-800">
+                        {customer.companyName}
+                      </td>
+                      <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                        {customer.email}
+                      </td>
+                      <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                        {customer.mobile}
+                      </td>
+                      <td className="px-3 py-4 whitespace-nowrap mt-3 text-sm text-gray-800">
+                        {customer.openingBalance}
+                      </td>
+                      <div
+                        style={{
+                          width: "100px",
+                          display: "flex",
+                          justifyContent: "center",
+                          marginTop: "10px",
+                        }}
+                      >
+                        <Dropdown
+                          overlay={
+                            <Menu style={{ width: "250px" }}>
+                              <Menu.Item
+                                key="1"
+                                onClick={() =>
+                                  handleViewCustomerDetails(customer.id)
+                                }
+                              >
+                                <EyeOutlined style={{ marginRight: "5px" }} />
+                                <span>View</span>
+                              </Menu.Item>
+                              <Menu.Item key="2">Action 2</Menu.Item>
+                              <Menu.Item key="3">Action 3</Menu.Item>
+                            </Menu>
+                          }
+                          trigger={["click"]}
+                        >
+                          <EllipsisVerticalIcon
+                            className="h-5 w-5 mt-3"
+                            aria-hidden="true"
+                          />
+                        </Dropdown>
+                      </div>
+                    </tr>
+                  </tbody>
+                ))}
+              </table>
+            </div>
+          )}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginRight: "40px",
+              marginTop: "10px",
+            }}
+          >
+            <div
+              style={{
+                textAlign: "center",
+                marginTop: "15px",
+                fontSize: "12px",
+                color: "#a1a1a1",
+              }}
+            >
+              Showing {indexOfFirstItem + 1} - {indexOfLastItem} of{" "}
+              {filteredCustomerList.length} results
+            </div>
+            <Pagination
+              current={currentPage}
+              total={filteredCustomerList.length}
+              pageSize={itemsPerPage}
+              onChange={paginate}
+            />
+          </div>
         </div>
       </div>
-    </div>
-
-    </>   
-
+    </>
   );
 };
 
